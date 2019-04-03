2332257af0024f01ae518e464cc4f95044b5b3d0
983708408@qq.com <983708408@qq.com>
2019-04-03 13:47:02 +0800

    add licence

--- super-devops-support/src/main/java/com/wl4g/devops/support/config/internal/logback/package-info.java
+++ super-devops-support/src/main/java/com/wl4g/devops/support/config/internal/logback/package-info.java
@@ -1,7 +1,16 @@
-/**
- * Ref: spring-boot-xx.jar:/org/springframework/boot/logging/logback/*
- * io.transport.TransportBootstrap: static {
- * System.setProperty(LoggingSystem.SYSTEM_PROPERTY,
- * "xxx.*.logback.LogbackLoggingSystem"); }
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.wl4g.devops.support.config.internal.logback;
--- super-devops-support/src/main/java/com/wl4g/devops/support/config/internal/package-info.java
+++ super-devops-support/src/main/java/com/wl4g/devops/support/config/internal/package-info.java
@@ -1,17 +1,16 @@
-/**
- * Spring cloud devops internal logger implements (server/console-ui) module.
- * <br/>
- * Thank you for the references: <br/>
- * https://www.jianshu.com/p/e20a5f42a395 <br/>
- * https://www.jianshu.com/p/734519d3c383 <br/>
- * https://www.jianshu.com/p/e20a5f42a395 <br/>
- * https://www.kancloud.cn/ahutchen/spring-boot-reference-guide/333233 <br/>
- * Implement custom exception notification (EG: Nails/WeChat/Email)
- * notifications：http://www.gdtarena.com/gdkc/javacxy/13554.html <br/>
- * 
- * @author Wangl.sir <983708408@qq.com>
- * @version v1.0
- * @date 2017年10月27日
- * @since
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.wl4g.devops.support.config.internal;
