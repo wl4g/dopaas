c8926fc96fdd1d3475aabd0407b20d4d325c5683
983708408@qq.com <983708408@qq.com>
2019-05-26 12:42:00 +0800

    Reserve djiango deploy provider

---
+++ super-devops-ci/super-devops-ci-core/src/main/java/com/wl4g/devops/ci/provider/DjangoStandardDeployProvider.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.wl4g.devops.ci.provider;
+
+import java.util.List;
+
+import com.wl4g.devops.ci.config.DeployProperties;
+import com.wl4g.devops.ci.service.DependencyService;
+import com.wl4g.devops.common.bean.ci.TaskDetail;
+import com.wl4g.devops.common.bean.scm.AppInstance;
+
+/**
+ * Django standard deployments provider.
+ * 
+ * @author Wangl.sir <983708408@qq.com>
+ * @version v1.0 2019年5月22日
+ * @since
+ */
+public class DjangoStandardDeployProvider extends BasedDeployProvider {
+
+	public DjangoStandardDeployProvider(DependencyService dependencyService, DeployProperties config, Integer projectId,
+			String path, String url, String branch, String alias, String tarPath, List<AppInstance> instances,
+			List<TaskDetail> taskDetails) {
+		super(dependencyService, config, projectId, path, url, branch, alias, tarPath, instances, taskDetails);
+	}
+
+	@Override
+	public void execute() throws Exception {
+
+	}
+
+}
