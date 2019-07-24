ba6ce17db441cc546d37a4d5e5bcd62cdc33fefb
983708408@qq.com <983708408@qq.com>
2019-07-25 00:48:07 +0800

    asfd

--- super-devops-iam/super-devops-iam-example/src/main/java/com/wl4g/devops/iam/example/android/AndroidIamClientController.java
+++ super-devops-iam/super-devops-iam-example/src/main/java/com/wl4g/devops/iam/example/android/AndroidIamClientController.java
@@ -15,7 +15,10 @@
  */
 package com.wl4g.devops.iam.example.android;
 
-import java.io.Serializable;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Collections;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
@@ -27,48 +30,66 @@ import java.util.concurrent.ConcurrentMap;
  * @version v1.0 2019年7月23日
  * @since
  */
-public class AndroidIamClientController implements Serializable {
-	final private static long serialVersionUID = 2737562691695613598L;
+public abstract class AndroidIamClientController {
+
+	final public static String PARAM_CODE = "code";
+	final public static String PARAM_DATA = "data";
+	final public static String CODE_UNAUTH = "401";
+	final public static String URI_AUTHENTICATOR = "authenticator";
 
 	/**
 	 * Table for storing authentication information.
 	 */
 	final protected ConcurrentMap<ServiceType, String> authTable = new ConcurrentHashMap<>(16);
 
-	/**
-	 * Singleton holder.
-	 * 
-	 * @author wangl.sir
-	 * @version v1.0 2019年7月23日
-	 * @since
-	 */
-	private static class SingletonHolder {
+	@SuppressWarnings("rawtypes")
+	protected abstract ResponseEntity doRequest(ServiceType serviceType, String requestUri, Map parameter);
 
-		/**
-		 * Singleton instance
-		 * 
-		 * @return
-		 */
-		final private static AndroidIamClientController INSTANCE = new AndroidIamClientController();
-
-		/**
-		 * Get singleton instance
-		 * 
-		 * @return
-		 */
-		synchronized static AndroidIamClientController getInstance() {
-			return INSTANCE;
+	@SuppressWarnings("rawtypes")
+	protected void preRequest(ServiceType serviceType, String requestUri, Map parameter) {
+		if (serviceType == null) {
+			throw new IllegalStateException("Service type must not be null");
 		}
-
 	}
 
-	/**
-	 * Get default IAM client controller instance.
-	 * 
-	 * @return
-	 */
-	public static AndroidIamClientController getDefault() {
-		return SingletonHolder.getInstance();
+	@SuppressWarnings("rawtypes")
+	protected boolean checkUnauthWithPostResponse(ServiceType serviceType, Map header, String body) {
+		if (serviceType == null) {
+			throw new IllegalStateException("Service type must not be null");
+		}
+		Map respBody = toJsonMap(body);
+		if (respBody != null && CODE_UNAUTH.equals(respBody.get(PARAM_CODE))) { // unauth?
+			if (ServiceType.IAM_SERV != serviceType) { // e.g. portal unauth?
+				String redirectUrl = (String) respBody.get(PARAM_DATA);
+				if (redirectUrl != null) {
+					// Check symbol for redirectUrl.
+					checkUrl(redirectUrl);
+
+					// Request iam
+					ResponseEntity resp = doRequest(ServiceType.IAM_SERV, URI_AUTHENTICATOR, Collections.emptyMap());
+					respBody = toJsonMap(resp.getBody());
+					// Iam unauthentication?
+					if (respBody != null && CODE_UNAUTH.equals(respBody.get(PARAM_CODE))) {
+						return true; // Jump login view.
+					}
+				}
+			} else { // iam unauth?
+				return true; // Direct jump login view.
+			}
+		}
+		return false;
+	}
+
+	@SuppressWarnings("rawtypes")
+	protected abstract Map toJsonMap(String json);
+
+	private void checkUrl(String url) {
+		// Check symbol for redirectUrl.
+		try {
+			new URI(url);
+		} catch (URISyntaxException e) {
+			throw new IllegalStateException(e);
+		}
 	}
 
 	/**
@@ -83,38 +104,128 @@ public class AndroidIamClientController implements Serializable {
 		/**
 		 * IAM certification background service.
 		 */
-		IAM_SERV,
+		IAM_SERV("http://passport.anjiancloud.test/sso/"),
 
 		/**
 		 * Management console back-end service.
 		 */
-		MP_SERV,
+		MP_SERV("http://mp.anjiancloud.test/mp/"),
 
 		/**
 		 * Portal backstage service.
 		 */
-		PORTAL_SERV,
+		PORTAL_SERV("http://portal.anjiancloud.test/portal/"),
 
 		/**
-		 * Energy consumption management background Service
+		 * Energy consumption management background service
 		 */
-		EMS_SERV,
+		EMS_SERV("http://ems.anjiancloud.test/ems/"),
 
 		/**
 		 * Trend forecasting background service.
 		 */
-		TRENDS_SERV,
+		TRENDS_SERV("http://trends.anjiancloud.test/trends/"),
 
 		/**
 		 * Family cloud backstage service
 		 */
-		HIOT_SERV,
+		HIOT_SERV("http://hiot.anjiancloud.test/hiot/"),
 
 		/**
-		 * Industrial internet of Things cloud background Service
+		 * Industrial internet of Things cloud background service
 		 */
-		IIOT_SERV;
+		IIOT_SERV("http://iiot.anjiancloud.test/iiot/");
 
+		// /**
+		// * IAM certification background service.
+		// */
+		// IAM_SERV("https://passport.anjiancloud.com/sso/"),
+		//
+		// /**
+		// * Management console back-end service.
+		// */
+		// MP_SERV("https://mp.anjiancloud.com/mp/"),
+		//
+		// /**
+		// * Portal backstage service.
+		// */
+		// PORTAL_SERV("https://portal.anjiancloud.com/portal/"),
+		//
+		// /**
+		// * Energy consumption management background service
+		// */
+		// EMS_SERV("https://ems.anjiancloud.com/ems/"),
+		//
+		// /**
+		// * Trend forecasting background service.
+		// */
+		// TRENDS_SERV("https://trends.anjiancloud.com/trends/"),
+		//
+		// /**
+		// * Family cloud backstage service
+		// */
+		// HIOT_SERV("https://hiot.anjiancloud.com/hiot/"),
+		//
+		// /**
+		// * Industrial internet of Things cloud background service
+		// */
+		// IIOT_SERV("https://iiot.anjiancloud.com/iiot/");
+
+		final private String baseUri;
+
+		private ServiceType(String baseUri) {
+			this.baseUri = baseUri;
+		}
+
+		public String getBaseUri() {
+			return baseUri;
+		}
+
+	}
+
+	@SuppressWarnings("rawtypes")
+	public static class ResponseEntity {
+
+		private Map header;
+
+		private String body;
+
+		public ResponseEntity(Map header) {
+			super();
+			this.header = header;
+		}
+
+		public ResponseEntity(String body) {
+			super();
+			this.body = body;
+		}
+
+		public ResponseEntity(Map header, String body) {
+			super();
+			this.header = header;
+			this.body = body;
+		}
+
+		public Map getHeader() {
+			return header;
+		}
+
+		public void setHeader(Map header) {
+			this.header = header;
+		}
+
+		public String getBody() {
+			return body;
+		}
+
+		public void setBody(String body) {
+			this.body = body;
+		}
+
+	}
+
+	public static void main(String[] args) {
+		System.out.println("".equals(null));
 	}
 
 }
