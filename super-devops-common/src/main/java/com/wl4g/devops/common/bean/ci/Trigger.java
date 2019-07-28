b5f39b4eae345ad36738e6b6dd79300e266ae27b
heweijie <babaa1f4@163.com>
2019-07-28 19:56:34 +0800

    task

--- super-devops-common/src/main/java/com/wl4g/devops/common/bean/ci/Trigger.java
+++ super-devops-common/src/main/java/com/wl4g/devops/common/bean/ci/Trigger.java
@@ -1,117 +1,69 @@
-/*
- * Copyright 2017 ~ 2025 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.wl4g.devops.common.bean.ci;
-
-import java.io.Serializable;
-import java.util.List;
 
 import com.wl4g.devops.common.bean.BaseBean;
 
+import java.io.Serializable;
+
 public class Trigger extends BaseBean implements Serializable {
 
-	private static final long serialVersionUID = 381411777614066880L;
+    private String name;
 
-	private Integer projectId;
+    private Integer groupId;
 
-	private String branchName;
+    private Integer taskId;
 
-	private String command;
+    private Integer type;
 
-	private Integer tarType;
+    private String cron;
 
-	private String projectName;
+    private String sha;
 
-	private Integer type;
+    public String getName() {
+        return name;
+    }
 
-	private String cron;
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
 
-	private String sha;
+    public Integer getGroupId() {
+        return groupId;
+    }
 
-	private List<TriggerDetail> triggerDetails;
+    public void setGroupId(Integer groupId) {
+        this.groupId = groupId;
+    }
 
-	public Integer getProjectId() {
-		return projectId;
-	}
+    public Integer getTaskId() {
+        return taskId;
+    }
 
-	public void setProjectId(Integer projectId) {
-		this.projectId = projectId;
-	}
+    public void setTaskId(Integer taskId) {
+        this.taskId = taskId;
+    }
 
-	public String getBranchName() {
-		return branchName;
-	}
+    public Integer getType() {
+        return type;
+    }
 
-	public void setBranchName(String branchName) {
-		this.branchName = branchName;
-	}
+    public void setType(Integer type) {
+        this.type = type;
+    }
 
-	public String getCommand() {
-		return command;
-	}
+    public String getCron() {
+        return cron;
+    }
 
-	public void setCommand(String command) {
-		this.command = command;
-	}
+    public void setCron(String cron) {
+        this.cron = cron == null ? null : cron.trim();
+    }
 
-	public Integer getTarType() {
-		return tarType;
-	}
+    public String getSha() {
+        return sha;
+    }
 
-	public void setTarType(Integer tarType) {
-		this.tarType = tarType;
-	}
+    public void setSha(String sha) {
+        this.sha = sha == null ? null : sha.trim();
+    }
 
-	public List<TriggerDetail> getTriggerDetails() {
-		return triggerDetails;
-	}
-
-	public void setTriggerDetails(List<TriggerDetail> triggerDetails) {
-		this.triggerDetails = triggerDetails;
-	}
-
-	public String getProjectName() {
-		return projectName;
-	}
-
-	public void setProjectName(String projectName) {
-		this.projectName = projectName;
-	}
-
-	public Integer getType() {
-		return type;
-	}
-
-	public void setType(Integer type) {
-		this.type = type;
-	}
-
-	public String getCron() {
-		return cron;
-	}
-
-	public void setCron(String cron) {
-		this.cron = cron;
-	}
-
-	public String getSha() {
-		return sha;
-	}
-
-	public void setSha(String sha) {
-		this.sha = sha;
-	}
-	
 }
