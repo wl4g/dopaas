687adf267b7596792da8309ba1aa8d9e92f626e0
heweijie <babaa1f4@163.com>
2019-08-05 11:37:32 +0800

    re create mapper

---
+++ super-devops-common/src/main/java/com/wl4g/devops/common/bean/umc/AlarmContactGroup.java
@@ -0,0 +1,20 @@
+package com.wl4g.devops.common.bean.umc;
+
+import com.wl4g.devops.common.bean.BaseBean;
+
+import java.io.Serializable;
+
+public class AlarmContactGroup extends BaseBean implements Serializable {
+    private static final long serialVersionUID = 381411777614066880L;
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+}
