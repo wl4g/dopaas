6c72fa3efb01b0743424b3d81d11a8b460b46a90
983708408@qq.com <983708408@qq.com>
2019-08-08 16:02:54 +0800

    update security default view

--- super-devops-iam/super-devops-iam-security/src/main/java/com/wl4g/devops/iam/web/DefaultViewController.java
+++ super-devops-iam/super-devops-iam-security/src/main/java/com/wl4g/devops/iam/web/DefaultViewController.java
@@ -22,21 +22,23 @@ import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.util.Assert;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import static com.wl4g.devops.common.constants.IAMDevOpsConstants.BEAN_DELEGATE_MSG_SOURCE;
+import static com.wl4g.devops.common.utils.web.WebUtils2.cleanURI;
+import static org.apache.commons.lang3.StringUtils.EMPTY;
+import static org.apache.commons.lang3.StringUtils.endsWithIgnoreCase;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.google.common.base.Charsets;
 import com.google.common.io.ByteStreams;
 import com.wl4g.devops.common.kit.jvm.JVMRuntimeKit;
-import com.wl4g.devops.common.utils.web.WebUtils2;
 import com.wl4g.devops.common.web.BaseController;
 import com.wl4g.devops.iam.common.i18n.SessionDelegateMessageBundle;
 import com.wl4g.devops.iam.config.IamProperties;
@@ -81,7 +83,7 @@ public class DefaultViewController extends BaseController {
 	 * @param filename
 	 * @param response
 	 */
-	@GetMapping(path = "{filename:.+}")
+	@GetMapping(path = "**/{filename:.+}")
 	public void readerView(@PathVariable("filename") String filename, HttpServletResponse response) throws Exception {
 		responseFile(null, filename, response);
 	}
@@ -92,28 +94,30 @@ public class DefaultViewController extends BaseController {
 	 * @param filename
 	 * @param response
 	 */
-	@GetMapping(path = URI_STATIC + "/{filename:.+}")
-	public void readerResource(@PathVariable("filename") String filename, HttpServletResponse response) throws Exception {
-		responseFile(URI_STATIC, filename, response);
+	@GetMapping(path = URI_STATIC + "/**")
+	public void readerResource(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String uri = request.getRequestURI();
+		String filepath = uri.substring(uri.indexOf(URI_STATIC) + URI_STATIC.length());
+		responseFile(URI_STATIC, filepath, response);
 	}
 
 	/**
 	 * Response file
 	 * 
 	 * @param basePath
-	 * @param filename
+	 * @param filepath
 	 * @param response
 	 * @throws Exception
 	 */
-	protected void responseFile(String basePath, String filename, HttpServletResponse response) throws Exception {
-		Assert.notNull(filename, "'filename' must not be null");
+	protected void responseFile(String basePath, String filepath, HttpServletResponse response) throws Exception {
+		Assert.notNull(filepath, "'filename' must not be null");
 		basePath = basePath == null ? "" : basePath;
 
 		// Get buffer cache
-		byte[] buffer = bufferCache.get(filename);
+		byte[] buffer = bufferCache.get(filepath);
 
 		if (buffer == null) {
-			Resource resource = getResource(basePath, filename);
+			Resource resource = getResource(basePath, filepath);
 			if (resource.exists()) {
 				if (log.isInfoEnabled()) {
 					log.info("Read file url:[{}]", resource.getURL());
@@ -122,7 +126,7 @@ public class DefaultViewController extends BaseController {
 
 				// Caching is enabled when in non-debug mode.
 				if (!JVMRuntimeKit.isJVMDebuging()) {
-					bufferCache.put(filename, buffer);
+					bufferCache.put(filepath, buffer);
 				}
 			}
 			// Not found
@@ -133,8 +137,8 @@ public class DefaultViewController extends BaseController {
 		}
 
 		// Response file buffer
-		String contentType = StringUtils.endsWithIgnoreCase(filename, "HTML") ? MediaType.TEXT_HTML_VALUE
-				: MediaType.TEXT_PLAIN_VALUE;
+		String contentType = endsWithIgnoreCase(filepath, "HTML") ? MediaType.TEXT_HTML_VALUE
+				: /* MediaType.TEXT_PLAIN_VALUE */EMPTY;
 		write(response, HttpStatus.OK.value(), contentType, buffer);
 	}
 
@@ -142,13 +146,13 @@ public class DefaultViewController extends BaseController {
 	 * Load resource file
 	 * 
 	 * @param basePath
-	 * @param filename
+	 * @param filepath
 	 * @return
 	 */
-	protected Resource getResource(String basePath, String filename) {
-		String location = new StringBuffer(config.getDefaultViewLoaderPath()).append(basePath).append("/").append(filename)
+	protected Resource getResource(String basePath, String filepath) {
+		String location = new StringBuffer(config.getDefaultViewLoaderPath()).append(basePath).append("/").append(filepath)
 				.toString();
-		return loader.getResource(WebUtils2.cleanURI(location));
+		return loader.getResource(cleanURI(location));
 	}
 
 }
