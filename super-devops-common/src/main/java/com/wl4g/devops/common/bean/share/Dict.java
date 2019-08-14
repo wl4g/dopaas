e8e8004c66a89e9a1e45a2404d682cc3076bd3ab
983708408@qq.com <983708408@qq.com>
2019-08-14 18:00:56 +0800

    optimize alarm

--- super-devops-common/src/main/java/com/wl4g/devops/common/bean/share/Dict.java
+++ super-devops-common/src/main/java/com/wl4g/devops/common/bean/share/Dict.java
@@ -5,95 +5,102 @@ import com.wl4g.devops.common.bean.BaseBean;
 import java.io.Serializable;
 
 public class Dict extends BaseBean implements Serializable {
+	private static final long serialVersionUID = -7546448616357790576L;
 
-    private String key;
+	private String key;
 
-    private String value;
+	private String value;
 
-    private String label;
+	private String label;
 
-    private String type;
+	private String type;
 
-    private String description;
+	private String description;
 
-    private String themes;
+	private String themes;
 
-    private String icon;
+	private String icon;
 
-    private Long sort;
+	private Long sort;
 
-    private Integer status;
+	private Integer status;
 
-    public String getKey() {
-        return key;
-    }
+	public String getKey() {
+		return key;
+	}
 
-    public void setKey(String key) {
-        this.key = key == null ? null : key.trim();
-    }
+	public void setKey(String key) {
+		this.key = key == null ? null : key.trim();
+	}
 
-    public String getValue() {
-        return value;
-    }
+	public String getValue() {
+		return value;
+	}
 
-    public void setValue(String value) {
-        this.value = value == null ? null : value.trim();
-    }
+	public void setValue(String value) {
+		this.value = value == null ? null : value.trim();
+	}
 
-    public String getLabel() {
-        return label;
-    }
+	public String getLabel() {
+		return label;
+	}
 
-    public void setLabel(String label) {
-        this.label = label == null ? null : label.trim();
-    }
+	public void setLabel(String label) {
+		this.label = label == null ? null : label.trim();
+	}
 
-    public String getType() {
-        return type;
-    }
+	public String getType() {
+		return type;
+	}
 
-    public void setType(String type) {
-        this.type = type == null ? null : type.trim();
-    }
+	public void setType(String type) {
+		this.type = type == null ? null : type.trim();
+	}
 
-    public String getDescription() {
-        return description;
-    }
+	public String getDescription() {
+		return description;
+	}
 
-    public void setDescription(String description) {
-        this.description = description == null ? null : description.trim();
-    }
+	public void setDescription(String description) {
+		this.description = description == null ? null : description.trim();
+	}
 
-    public String getThemes() {
-        return themes;
-    }
+	public String getThemes() {
+		return themes;
+	}
 
-    public void setThemes(String themes) {
-        this.themes = themes == null ? null : themes.trim();
-    }
+	public void setThemes(String themes) {
+		this.themes = themes == null ? null : themes.trim();
+	}
 
-    public String getIcon() {
-        return icon;
-    }
+	public String getIcon() {
+		return icon;
+	}
 
-    public void setIcon(String icon) {
-        this.icon = icon == null ? null : icon.trim();
-    }
+	public void setIcon(String icon) {
+		this.icon = icon == null ? null : icon.trim();
+	}
 
-    public Long getSort() {
-        return sort;
-    }
+	public Long getSort() {
+		return sort;
+	}
 
-    public void setSort(Long sort) {
-        this.sort = sort;
-    }
+	public void setSort(Long sort) {
+		this.sort = sort;
+	}
 
-    public Integer getStatus() {
-        return status;
-    }
+	public Integer getStatus() {
+		return status;
+	}
 
-    public void setStatus(Integer status) {
-        this.status = status;
-    }
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	@Override
+	public String toString() {
+		return "Dict [key=" + key + ", value=" + value + ", label=" + label + ", type=" + type + ", description=" + description
+				+ ", themes=" + themes + ", icon=" + icon + ", sort=" + sort + ", status=" + status + "]";
+	}
 
 }
