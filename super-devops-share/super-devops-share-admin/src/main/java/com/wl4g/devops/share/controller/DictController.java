98755bde0de252d664057a3d129aa2acb6ca9e6e
heweijie <babaa1f4@163.com>
2019-08-15 13:57:52 +0800

    dict

--- super-devops-share/super-devops-share-admin/src/main/java/com/wl4g/devops/share/controller/DictController.java
+++ super-devops-share/super-devops-share-admin/src/main/java/com/wl4g/devops/share/controller/DictController.java
@@ -1,18 +1,25 @@
 package com.wl4g.devops.share.controller;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
-import com.wl4g.devops.share.service.DictService;
 import com.wl4g.devops.common.bean.scm.CustomPage;
 import com.wl4g.devops.common.bean.share.Dict;
+import com.wl4g.devops.common.utils.serialize.JacksonUtils;
 import com.wl4g.devops.common.web.BaseController;
 import com.wl4g.devops.common.web.RespBase;
 import com.wl4g.devops.dao.share.DictDao;
+import com.wl4g.devops.share.service.DictService;
+import com.wl4g.devops.support.cache.JedisService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
+
+import static com.wl4g.devops.common.constants.ShareDevOpsConstants.REDIS_DICTS_CACHE;
 
 /**
  * 字典
@@ -29,6 +36,9 @@ public class DictController extends BaseController {
 
 	@Autowired
 	private DictService dictService;
+
+	@Autowired
+	private JedisService jedisService;
 
 	@RequestMapping(value = "/list")
 	public RespBase<?> list(CustomPage customPage,String key,String label,String type,String description) {
@@ -91,5 +101,24 @@ public class DictController extends BaseController {
 		resp.getData().put("list", list);
 		return resp;
 	}
+
+	@RequestMapping(value = "/cache")
+	public RespBase<?> cache() {
+		RespBase<Object> resp = RespBase.create();
+		//get from redis first , not found then find from db
+		String s = jedisService.get(REDIS_DICTS_CACHE);
+		Map<String, Object> result;
+		if(StringUtils.isNotBlank(s)){
+			result = JacksonUtils.parseJSON(s, new TypeReference<Map<String, Object>>() {});
+		}else{
+			result = dictService.cache();
+			//cache to redis
+			String s1 = JacksonUtils.toJSONString(result);
+			jedisService.set(REDIS_DICTS_CACHE,s1,0);
+		}
+		resp.setData(result);
+		return resp;
+	}
+
 
 }
