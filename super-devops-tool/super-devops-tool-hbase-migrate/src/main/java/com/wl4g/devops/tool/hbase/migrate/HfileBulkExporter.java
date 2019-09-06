7fec75d0895019609786ce75144b953b6af2dead
heweijie <babaa1f4@163.com>
2019-09-06 16:50:01 +0800

    genfile

--- super-devops-tool/super-devops-tool-hbase-migrate/src/main/java/com/wl4g/devops/tool/hbase/migrate/HfileBulkExporter.java
+++ super-devops-tool/super-devops-tool-hbase-migrate/src/main/java/com/wl4g/devops/tool/hbase/migrate/HfileBulkExporter.java
@@ -1,5 +1,6 @@
 package com.wl4g.devops.tool.hbase.migrate;
 
+import com.wl4g.devops.tool.common.utils.Assert;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.HelpFormatter;
@@ -13,21 +14,12 @@ import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.Connection;
 import org.apache.hadoop.hbase.client.ConnectionFactory;
 import org.apache.hadoop.hbase.client.Put;
-import org.apache.hadoop.hbase.client.Scan;
 import org.apache.hadoop.hbase.client.Table;
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
 import org.apache.hadoop.hbase.mapreduce.HFileOutputFormat2;
 import org.apache.hadoop.hbase.mapreduce.TableInputFormat;
-import org.apache.hadoop.hbase.protobuf.ProtobufUtil;
-import org.apache.hadoop.hbase.protobuf.generated.ClientProtos;
-import org.apache.hadoop.hbase.util.Base64;
-import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-
-import com.wl4g.devops.tool.common.utils.Assert;
-
-import static org.apache.commons.lang3.StringUtils.isNotBlank;
 
 import java.net.URI;
 
@@ -49,8 +41,8 @@ public class HfileBulkExporter {
 		options.addOption("T", "tmpdir", false, "Hbase tmp directory. default:" + DEFAULT_HBASE_FSTMP_DIR);
 		options.addRequiredOption("z", "zkaddr", true, "Zookeeper address.");
 		options.addRequiredOption("t", "tabname", true, "Hbase table name.");
-		options.addRequiredOption("p", "path", true,
-				"Data hdfs path to be import. e.g. hdfs://localhost:9000/bak/safeclound.tb_air");
+		/*options.addRequiredOption("p", "path", true,
+				"Data hdfs path to be import. e.g. hdfs://localhost:9000/bak/safeclound.tb_air");*/
 		options.addRequiredOption("o", "output", true, "Output hdfs path.");
 		options.addOption("b", "batchsize", false, "Scan batch size. default: " + DEFAULT_SCAN_BATCH_SIZE);
 		options.addOption("s", "startrow", false, "Scan start rowkey.");
@@ -76,51 +68,51 @@ public class HfileBulkExporter {
 		// Check directory.
 		String output = line.getOptionValue("o");
 		FileSystem fs = FileSystem.get(new URI(output), new Configuration(), "root");
-		Assert.state(fs.exists(new Path(output)), String.format("Catalogs do not allow other data. '%s'", output));
+		Assert.state(!fs.exists(new Path(output)), String.format("Catalogs do not allow other data. '%s'", output));
 
 		// Scan condition.
-		String startRow = line.getOptionValue("s");
+		/*String startRow = line.getOptionValue("s");
 		String endRow = line.getOptionValue("e");
 		String startTime = line.getOptionValue("S");
 		String endTime = line.getOptionValue("E");
 		Scan scan = new Scan(); // If necessary
-//		if (null != startTimestamp || null != endTimestamp) {
-//			startTimestamp = startTimestamp == null ? 0 : startTimestamp;
-//			endTimestamp = endTimestamp == null ? System.currentTimeMillis() : endTimestamp;
-//			conf.set(TableInputFormat.SCAN_TIMERANGE_START, startTimestamp.toString());
-//			conf.set(TableInputFormat.SCAN_TIMERANGE_END, endTimestamp.toString());
-//			scan.setTimeRange(startTime, endTime);
-//		}
-//
-//		if (isNotBlank(startRow)) {
-//			conf.set(TableInputFormat.SCAN_ROW_START, startRow);
-//			scan.setStartRow(Bytes.toBytes(startRow));
-//		}
-//		if (isNotBlank(endRow)) {
-//			conf.set(TableInputFormat.SCAN_ROW_STOP, endRow);
-//			scan.setStopRow(Bytes.toBytes(endRow));
-//		}
-//		System.out.println("filter:" + startTimestamp + "--" + endTimestamp + "--" + startRow + "--" + endRow);
-//		ClientProtos.Scan proto = ProtobufUtil.toScan(scan);
-//		conf.set(TableInputFormat.SCAN, Base64.encodeBytes(proto.toByteArray()));
-//
-//		Connection conn = ConnectionFactory.createConnection(conf);
-//		Table table = conn.getTable(TableName.valueOf(line.getOptionValue("t")));
-//		Job job = Job.getInstance(conf);
-//		job.setJarByClass(HfileBulkExporter.class);
-//		job.setMapperClass(SimpleHfileTransformMapper.class);
-//		job.setInputFormatClass(TableInputFormat.class);
-//		job.setMapOutputKeyClass(ImmutableBytesWritable.class);
-//		job.setMapOutputValueClass(Put.class);
-//		// job.getConfiguration().set("mapred.jar",
-//		// mapredJar);//预先将程序打包再将jar分发到集群上
-//
-//		HFileOutputFormat2.configureIncrementalLoad(job, table,
-//				conn.getRegionLocator(TableName.valueOf(line.getOptionValue("t"))));
-//		FileOutputFormat.setOutputPath(job, new Path(output));
-//		if (job.waitForCompletion(true)) {
-//			log.info("Exported to successfully !");
-//		}
+		if (null != startTimestamp || null != endTimestamp) {
+			startTimestamp = startTimestamp == null ? 0 : startTimestamp;
+			endTimestamp = endTimestamp == null ? System.currentTimeMillis() : endTimestamp;
+			conf.set(TableInputFormat.SCAN_TIMERANGE_START, startTimestamp.toString());
+			conf.set(TableInputFormat.SCAN_TIMERANGE_END, endTimestamp.toString());
+			scan.setTimeRange(startTime, endTime);
+		}
+
+		if (isNotBlank(startRow)) {
+			conf.set(TableInputFormat.SCAN_ROW_START, startRow);
+			scan.setStartRow(Bytes.toBytes(startRow));
+		}
+		if (isNotBlank(endRow)) {
+			conf.set(TableInputFormat.SCAN_ROW_STOP, endRow);
+			scan.setStopRow(Bytes.toBytes(endRow));
+		}
+		System.out.println("filter:" + startTimestamp + "--" + endTimestamp + "--" + startRow + "--" + endRow);
+		ClientProtos.Scan proto = ProtobufUtil.toScan(scan);
+		conf.set(TableInputFormat.SCAN, Base64.encodeBytes(proto.toByteArray()));*/
+
+		Connection conn = ConnectionFactory.createConnection(conf);
+		Table table = conn.getTable(TableName.valueOf(line.getOptionValue("t")));
+		Job job = Job.getInstance(conf);
+		job.setJarByClass(HfileBulkExporter.class);
+		job.setMapperClass(SimpleHfileTransformMapper.class);
+		job.setInputFormatClass(TableInputFormat.class);
+		job.setMapOutputKeyClass(ImmutableBytesWritable.class);
+		job.setMapOutputValueClass(Put.class);
+		// job.getConfiguration().set("mapred.jar",
+		// mapredJar);//预先将程序打包再将jar分发到集群上
+
+		HFileOutputFormat2.configureIncrementalLoad(job, table,
+				conn.getRegionLocator(TableName.valueOf(line.getOptionValue("t"))));
+		FileOutputFormat.setOutputPath(job, new Path(output));
+		if (job.waitForCompletion(true)) {
+			log.info("Exported to successfully !");
+		}
 
 	}
 
--- super-devops-tool/super-devops-tool-hbase-migrate/src/main/java/com/wl4g/devops/tool/hbase/migrate/SimpleHfileTransformMapper.java
+++ super-devops-tool/super-devops-tool-hbase-migrate/src/main/java/com/wl4g/devops/tool/hbase/migrate/SimpleHfileTransformMapper.java
@@ -1,6 +1,7 @@
 package com.wl4g.devops.tool.hbase.migrate;
 
 import org.apache.hadoop.hbase.Cell;
+import org.apache.hadoop.hbase.CellUtil;
 import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
@@ -8,44 +9,135 @@ import org.apache.hadoop.hbase.mapreduce.TableMapper;
 import org.apache.hadoop.hbase.util.Bytes;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * HBASE hfile transform mapper.
- * 
+ *
  * @author Wangl.sir
  * @version v1.0 2019年9月6日
  * @since
  */
 public class SimpleHfileTransformMapper extends TableMapper<ImmutableBytesWritable, Put> {
 
-	public void map(ImmutableBytesWritable row, Result value, Context context) throws IOException, InterruptedException {
-		// this example is just copying the data from the source table...
-		context.write(row, resultToPut(row, value));
-	}
 
-	private static Put resultToPut(ImmutableBytesWritable key, Result result) throws IOException {
+    private static List<String> list = new ArrayList<>();
+
+    private static long count = 0;
+
+    static {
+        list.add("11111112");
+        list.add("11111114");
+        list.add("11111115");
+        list.add("11111117");
+        list.add("11111118");
+        list.add("11111119");
+        list.add("11111120");
+        list.add("11111121");
+        list.add("11111122");
+        list.add("11111123");
+        list.add("11111124");
+        list.add("11111131");
+        list.add("11111132");
+        list.add("11111135");
+        list.add("11111136");
+        list.add("11111137");
+        list.add("11111138");
+        list.add("11111139");
+        list.add("11111140");
+        list.add("11111141");
+        list.add("11111142");
+        list.add("11111143");
+        list.add("11111144");
+        list.add("11111145");
+        list.add("11111146");
+        list.add("11111147");
+        list.add("11111148");
+        list.add("11111149");
+        list.add("11111150");
+        list.add("11111151");
+        list.add("11111152");
+        list.add("11111153");
+        list.add("11111154");
+        list.add("11111155");
+        list.add("11111156");
+        list.add("11111157");
+        list.add("11111158");
+        list.add("11111159");
+        list.add("11111160");
+        list.add("11111161");
+        list.add("11111171");
+        list.add("11111172");
+        list.add("11111173");
+        list.add("11111174");
+        list.add("11111175");
+        list.add("11111176");
+        list.add("11111177");
+        list.add("11111178");
+        list.add("11111179");
+        list.add("11111180");
+        list.add("11111181");
+        list.add("11111182");
+    }
+
+
+    public void map(ImmutableBytesWritable row, Result value, Context context) throws IOException, InterruptedException {
+        // this example is just copying the data from the source table...
+        Put put = resultToPut(row, value);
+        if(null!=put){
+            context.write(row, put);
+        }
+
+
+    }
+
+	/*private static Put resultToPut(ImmutableBytesWritable key, Result result) throws IOException {
 		Put put = new Put(Bytes.toBytes(Bytes.toString(key.get())));
 		for (Cell cell : result.listCells()) {
 			put.add(cell);
 		}
 		return put;
-	}
+	}*/
 
-	/**
-	 * 截取byte数组 不改变原数组
-	 * 
-	 * @param b
-	 *            原数组
-	 * @param off
-	 *            偏差值（索引）
-	 * @param length
-	 *            长度
-	 * @return 截取后的数组
-	 */
-	public static byte[] subByte(byte[] b, int off, int length) {
-		byte[] b1 = new byte[length];
-		System.arraycopy(b, off, b1, 0, length);
-		return b1;
-	}
+    private static Put resultToPut(ImmutableBytesWritable key, Result result) throws IOException {
+        count++;
+        System.out.println(count);
+
+        String oldRowKey = Bytes.toString(key.get());
+        String pre = oldRowKey.substring(0, 8);
+        if (list.contains(pre)) {
+            String newRowKey = "3" + oldRowKey.substring(1, oldRowKey.length());//TODO 修改开头数字
+            Put put = new Put(Bytes.toBytes(newRowKey));
+            for (Cell cell : result.listCells()) {
+                byte[] family = subByte(cell.getFamilyArray(), cell.getFamilyOffset(), cell.getFamilyLength());
+                byte[] value = subByte(cell.getValueArray(), cell.getValueOffset(), cell.getValueLength());
+                byte[] qualifier = subByte(cell.getQualifierArray(), cell.getQualifierOffset(), cell.getQualifierLength());
+                long timestamp = cell.getTimestamp();
+                byte type = cell.getTypeByte();
+                byte[] newRowKeyByte = Bytes.toBytes(newRowKey);
+                Cell newCell = CellUtil.createCell(newRowKeyByte, family, qualifier, timestamp, type, value);
+                put.add(newCell);
+            }
+            return put;
+        }
+
+        return null;
+    }
+
+    /**
+     * 截取byte数组 不改变原数组
+     *
+     * @param b      原数组
+     * @param off    偏差值（索引）
+     * @param length 长度
+     * @return 截取后的数组
+     */
+    public static byte[] subByte(byte[] b, int off, int length) {
+        byte[] b1 = new byte[length];
+        System.arraycopy(b, off, b1, 0, length);
+        return b1;
+    }
+
 
 }
