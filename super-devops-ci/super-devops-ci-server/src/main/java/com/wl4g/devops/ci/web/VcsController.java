de77aa34f7e2d0488014acd4e1be6d549919d97c
983708408@qq.com <983708408@qq.com>
2019-11-12 17:26:11 +0800

    optimization

--- super-devops-ci/super-devops-ci-server/src/main/java/com/wl4g/devops/ci/web/VcsController.java
+++ super-devops-ci/super-devops-ci-server/src/main/java/com/wl4g/devops/ci/web/VcsController.java
@@ -24,8 +24,7 @@ public class VcsController extends BaseController {
 	@RequestMapping("/list")
 	public RespBase<?> list(PageModel pm) {
 		RespBase<Object> resp = RespBase.create();
-		resp.buildMap().put("list", vcsService.list(pm));
-		resp.buildMap().put("page", pm);
+		resp.buildMap().andPut("list", vcsService.list(pm)).andPut("page", pm);
 		return resp;
 	}
 
