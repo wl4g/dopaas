0696f566249c0ddee43fab9e445e7ebf783ffeab
heweijie <babaa1f4@163.com>
2019-11-12 10:09:11 +0800

    git demo

--- super-devops-ci/super-devops-ci-core/src/main/java/com/wl4g/devops/ci/vcs/Test.java
+++ super-devops-ci/super-devops-ci-core/src/main/java/com/wl4g/devops/ci/vcs/Test.java
@@ -6,11 +6,12 @@ import com.jcraft.jsch.Session;
 import org.eclipse.jgit.api.CloneCommand;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.TransportConfigCallback;
-import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.transport.*;
 import org.eclipse.jgit.util.FS;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 /**
  * @author vjay
@@ -19,27 +20,50 @@ import java.io.File;
 public class Test {
 
 
-    public static void main(String[] args) throws GitAPIException {
+    public static void main(String[] args) throws Exception {
+
+        /*KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        keyPairGenerator.initialize(2048, new SecureRandom());
+        PrivateKey privateKey = keyPairGenerator.genKeyPair().getPrivate();
+
+        StringWriter writer = new StringWriter();
+
+        try (PEMWriter pemWriter = new PEMWriter(writer)) {
+            pemWriter.writeObject(privateKey);
+        }
+
+        String privateKeyStr = writer.toString();
+        System.out.println(privateKeyStr);
+        KeyPair load = KeyPair.load(null, privateKeyStr.getBytes("US-ASCII"), null);*/
+
+
         SshSessionFactory sshSessionFactory = new JschConfigSessionFactory() {
+
+
+
             @Override
             protected void configure(OpenSshConfig.Host hc, Session session) {
-                //session.setConfig("StrictHostKeyChecking","no");
+                session.setConfig("StrictHostKeyChecking","no");
             }
 
             @Override
             protected JSch createDefaultJSch( FS fs ) throws JSchException {
                 JSch defaultJSch = super.createDefaultJSch( fs );
                 defaultJSch.removeAllIdentity();
-                defaultJSch.addIdentity( "/Users/vjay/.ssh/ci_id_rsa" );
+                defaultJSch.addIdentity( "/Users/vjay/.ssh/id_rsa");
                 defaultJSch.setKnownHosts("/Users/vjay/.ssh/known_hosts");
-                //defaultJSch.getIdentityRepository().add(Files.readBytes(new File("/")));
+               /* try {
+                    defaultJSch.getIdentityRepository().add(getContent("/Users/vjay/.ssh/gitlab-rsa"));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }*/
                 return defaultJSch;
             }
 
 
         };
         CloneCommand cloneCommand = Git.cloneRepository();
-        cloneCommand.setURI( "heweijie@git.anjiancloud.repo:biz-team/portal-team/safecloud-web-portal.git" );
+        cloneCommand.setURI( "git@10.0.0.189:root/anzhuotest.git" );
         cloneCommand.setDirectory(new File("/Users/vjay/citest"));
         cloneCommand.setTransportConfigCallback( new TransportConfigCallback() {
             @Override
@@ -52,35 +76,30 @@ public class Test {
     }
 
 
-    public static class MySShSessionFactory extends JschConfigSessionFactory {
-
-        private String sshKeyFilePath;
-
-        @Override
-        protected void configure(OpenSshConfig.Host hc, Session session) {
-            //TODO Auto-generated method stub
-            java.util.Properties config = new java.util.Properties();
-            config.put("StrictHostKeyChecking", "no");
-            session.setConfig(config);
+    public static byte[] getContent(String filePath) throws IOException {
+        File file = new File(filePath);
+        long fileSize = file.length();
+        if (fileSize > Integer.MAX_VALUE) {
+            return null;
         }
-
-        @Override
-        protected JSch getJSch(final OpenSshConfig.Host hc, FS fs) throws JSchException {
-            JSch jsch = new JSch();
-            jsch.removeAllIdentity();
-            jsch.addIdentity(sshKeyFilePath);
-            jsch.setKnownHosts("C:\\known_hosts");
-            return jsch;
+        FileInputStream fi = new FileInputStream(file);
+        byte[] buffer = new byte[(int) fileSize];
+        int offset = 0;
+        int numRead = 0;
+        while (offset < buffer.length
+                && (numRead = fi.read(buffer, offset, buffer.length - offset)) >= 0) {
+            offset += numRead;
         }
-
-        public String getSshKeyFilePath() {
-            return sshKeyFilePath;
+        // 确保所有数据均被读取
+        if (offset != buffer.length) {
+            throw new IOException("Could not completely read file "
+                    + file.getName());
         }
-
-        public void setSshKeyFilePath(String sshKeyFilePath) {
-            this.sshKeyFilePath = sshKeyFilePath;
-        }
+        fi.close();
+        return buffer;
     }
+
+
 
 
 }
