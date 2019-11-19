c9267208cae6e44d666d652bbc6b2ba43f73a19a
heweijie <babaa1f4@163.com>
2019-11-19 15:18:20 +0800

    group add type

---
+++ super-devops-common/src/main/java/com/wl4g/devops/common/bean/iam/Company.java
@@ -0,0 +1,67 @@
+package com.wl4g.devops.common.bean.iam;
+
+import com.wl4g.devops.common.bean.BaseBean;
+
+import java.io.Serializable;
+
+public class Company extends BaseBean implements Serializable {
+    private Integer id;
+
+    private Integer groupId;
+
+    private String displayName;
+
+    private String contact;
+
+    private String contactPhone;
+
+    private String address;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(Integer groupId) {
+        this.groupId = groupId;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName == null ? null : displayName.trim();
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact == null ? null : contact.trim();
+    }
+
+    public String getContactPhone() {
+        return contactPhone;
+    }
+
+    public void setContactPhone(String contactPhone) {
+        this.contactPhone = contactPhone == null ? null : contactPhone.trim();
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address == null ? null : address.trim();
+    }
+}
