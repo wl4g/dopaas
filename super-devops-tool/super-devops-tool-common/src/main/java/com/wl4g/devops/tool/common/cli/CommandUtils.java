0eba3080643f760e2210df009e68cb360b1cd607
983708408@qq.com <983708408@qq.com>
2019-12-29 18:37:46 +0800

    optimzie opencv videoFaceTests

--- super-devops-tool/super-devops-tool-common/src/main/java/com/wl4g/devops/tool/common/cli/CommandUtils.java
+++ super-devops-tool/super-devops-tool-common/src/main/java/com/wl4g/devops/tool/common/cli/CommandUtils.java
@@ -15,9 +15,10 @@
  */
 package com.wl4g.devops.tool.common.cli;
 
+import static com.wl4g.devops.tool.common.log.SmartLoggerFactory.getLogger;
+import static java.util.Arrays.asList;
 import static java.util.stream.Collectors.toList;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.apache.commons.cli.BasicParser;
@@ -27,12 +28,27 @@ import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.wl4g.devops.tool.common.lang.Assert2;
 
+/**
+ * Command utility.
+ * 
+ * @author wanglsir@gmail.com, 983708408@qq.com
+ * @version 2019年12月29日 v1.0.0
+ * @see
+ */
 @SuppressWarnings("deprecation")
 public class CommandUtils {
+
+	/**
+	 * New create builder. {@link Builder}
+	 * 
+	 * @return
+	 */
+	public final static Builder newBuilder() {
+		return new Builder();
+	}
 
 	/**
 	 * Command line builder tool.
@@ -41,8 +57,8 @@ public class CommandUtils {
 	 * @version v1.0.0 2019-09-08
 	 * @since
 	 */
-	public static class Builder {
-		final protected Logger log = LoggerFactory.getLogger(getClass());
+	public final static class Builder {
+		final protected Logger log = getLogger(getClass());
 
 		private Options options;
 
@@ -67,18 +83,25 @@ public class CommandUtils {
 			return this;
 		}
 
+		/**
+		 * Build parsing required options.
+		 * 
+		 * @param args
+		 * @return
+		 * @throws ParseException
+		 */
 		public CommandLine build(String args[]) throws ParseException {
 			CommandLine line = null;
 			try {
 				line = new BasicParser().parse(options, args);
 				if (log.isInfoEnabled()) {
 					// Print input argument list.
-					List<String> printArgs = Arrays.asList(line.getOptions()).stream()
+					List<String> printArgs = asList(line.getOptions()).stream()
 							.map(o -> o.getOpt() + "|" + o.getLongOpt() + "=" + o.getValue()).collect(toList());
-					log.info(String.format("Parsed arguments: %s", printArgs));
+					log.info("Parsed arguments: {}", printArgs);
 				}
 			} catch (Exception e) {
-				new HelpFormatter().printHelp("\t", options);
+				new HelpFormatter().printHelp("\n", options);
 				throw new ParseException(e.getMessage());
 			}
 			return line;
