2d23daf7dff8a7888b0cb81a24bc1c2b1e9a8357
983708408@qq.com <983708408@qq.com>
2020-01-17 22:07:12 +0800

    a

--- super-devops-shell/super-devops-shell-cli/src/main/java/Test1.java
+++ super-devops-shell/super-devops-shell-cli/src/main/java/Test1.java
@@ -21,19 +21,23 @@ public class Test1 {
 
 processBar()
 {
-    let process=$1
-    let whole=$2
+    process=$1 # 当前进度
+    whole=$2 # 总进度数
+    # 百分比比值(小数)
+    percent_ratio=`awk BEGIN'{printf "%.2f", ('$process'/'$whole')}'`
+    # 百分比数值
+    percent=`awk BEGIN'{printf "%d", (100*'$percent_ratio')}'`
     let index=$((${process}%4))
     arr=( "|" "/" "-" "\\" )
     bar='>'
-    for((i=0;i<process-1;i++))
+    for((i=0;i<($percent-1)/2;i++))
     do
         bar="="$bar
     done
-    printf "[%-100s][%d%%][%3d/%03d][\e[43;46;1m%c\e[0m]\r" $bar $process $process $whole "${arr[$index]}"
+    printf "[%-50s][%d%%][%3d/%03d][%c]\r" $bar $percent $process $whole "${arr[$index]}"
 }
 
-whole=100
+whole=200
 process=0
 while [ $process -lt $whole ]
 do
@@ -44,8 +48,6 @@ done
 printf "\n"
 
 
-
-[===============================================================================>][100%][100/100][|]
-
+[=================================================>][100%][200/200][|]
 
 */
