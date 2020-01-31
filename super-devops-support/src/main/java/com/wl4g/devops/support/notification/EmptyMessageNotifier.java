63a88728bd09cc08fa23a5b009dfe2635ce03211
983708408@qq.com <983708408@qq.com>
2020-01-31 13:57:15 +0800

    recovery emptyMessageNotifier

---
+++ super-devops-support/src/main/java/com/wl4g/devops/support/notification/EmptyMessageNotifier.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2017 ~ 2025 the original author or authors. <wanglsir@gmail.com, 983708408@qq.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.wl4g.devops.support.notification;
+
+public class EmptyMessageNotifier extends AbstractMessageNotifier<Object, NotifyMessage> {
+
+	public EmptyMessageNotifier(Object config) {
+		super(config);
+	}
+
+	@Override
+	public NotifierKind kind() {
+		return NotifierKind.Empty;
+	}
+
+	@Override
+	public void send(NotifyMessage message) {
+		throw new UnsupportedOperationException(
+				"This is an empty message notifier implementation. Please check whether the real message notifier is configured correctly!");
+	}
+
+	@SuppressWarnings({ "unchecked" })
+	@Override
+	public Object sendForReply(NotifyMessage message) {
+		throw new UnsupportedOperationException(
+				"This is an empty message notifier implementation. Please check whether the real message notifier is configured correctly!");
+	}
+
+}
