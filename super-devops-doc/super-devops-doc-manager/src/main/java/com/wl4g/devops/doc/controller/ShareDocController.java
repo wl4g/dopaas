63bbe02d87d8db0c2b8cd444e5bd9ba221a7a56c
heweijie <babaa1f4@163.com>
2020-02-18 14:22:02 +0800

    add shareType

--- super-devops-doc/super-devops-doc-manager/src/main/java/com/wl4g/devops/doc/controller/ShareDocController.java
+++ super-devops-doc/super-devops-doc-manager/src/main/java/com/wl4g/devops/doc/controller/ShareDocController.java
@@ -4,26 +4,39 @@ package com.wl4g.devops.doc.controller;
 import com.wl4g.devops.common.bean.doc.FileChanges;
 import com.wl4g.devops.common.web.RespBase;
 import com.wl4g.devops.doc.service.FileService;
-import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static com.wl4g.devops.common.web.RespBase.RetCode.NOT_FOUND_ERR;
+import static com.wl4g.devops.common.web.RespBase.RetCode.UNAUTHC;
+import static com.wl4g.devops.tool.common.log.SmartLoggerFactory.getLogger;
+import static java.util.Objects.isNull;
+import static org.apache.commons.lang3.StringUtils.equalsIgnoreCase;
+
 @RestController
 @RequestMapping("/doc")
 public class ShareDocController {
+
+    final protected Logger log = getLogger(getClass());
 
     @Autowired
     private FileService fileService;
 
     @CrossOrigin
-    @RequestMapping(value = "/read")
-    public RespBase<?> list(String code, String passwd) {
+    @RequestMapping(value = "/rendering")
+    public RespBase<?> rendering(String code, String passwd) {
+        log.info("rendering file code={} passwd={}",code,passwd);
         RespBase<Object> resp = RespBase.create();
         FileChanges lastByFileCode = fileService.getLastByFileCode(code);
-        if(StringUtils.isNotBlank(lastByFileCode.getPasswd()) && !lastByFileCode.getPasswd().equalsIgnoreCase(passwd)){
-            resp.setCode(RespBase.RetCode.UNAUTHC);
+
+        if(isNull(lastByFileCode.getShareType()) || lastByFileCode.getShareType()==-1){//not share yet
+            resp.setCode(NOT_FOUND_ERR);
+            return resp;
+        }else if(lastByFileCode.getShareType()==1 && !equalsIgnoreCase(lastByFileCode.getPasswd(),passwd)){
+            resp.setCode(UNAUTHC);
             return resp;
         }
         resp.setData(lastByFileCode.getContent());
