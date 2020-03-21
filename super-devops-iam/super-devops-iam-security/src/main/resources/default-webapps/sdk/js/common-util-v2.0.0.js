e67808472a8bcf5255b2b5fc695fc254fc24a04e
983708408@qq.com <983708408@qq.com>
2020-03-21 12:28:42 +0800

    optimzie iamjsdk stage1

--- super-devops-iam/super-devops-iam-security/src/main/resources/default-webapps/sdk/js/common-util-v2.0.0.js
+++ super-devops-iam/super-devops-iam-security/src/main/resources/default-webapps/sdk/js/common-util-v2.0.0.js
@@ -10,10 +10,10 @@
 
 	// Common util.
 	window.Common.Util = {
-		printWarn: function(msg){
-			console.log("%cSecurity Warning!","font-size:50px;color:red;-webkit-text-fill-color:red;-webkit-text-stroke:1px black;");
+		printSafeWarn: function(msg){
+			console.log("%cSECURITY WARNING!","font-size:50px;color:red;-webkit-text-fill-color:red;-webkit-text-stroke:1px black;");
 		 	if(!Common.Util.isEmpty(msg)){
-				console.log("%c"+msg,"font-size:16px;color:#303d65;");
+				console.log("%c"+msg,"font-size:14px;color:#303d65;");
 		 	}
 		},
 		getEleValue: function(name, obj){
