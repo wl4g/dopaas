f48842bf825fd3dd51bd153c4c7a3e53cb5aba76
heweijie <babaa1f4@163.com>
2020-04-15 12:38:25 +0800

    ci branch

--- super-devops-ci/super-devops-ci-core/src/main/java/com/wl4g/devops/ci/web/TaskController.java
+++ super-devops-ci/super-devops-ci-core/src/main/java/com/wl4g/devops/ci/web/TaskController.java
@@ -172,10 +172,10 @@ public class TaskController extends BaseController {
 
 	@RequestMapping(value = "/getDependencys")
 	@RequiresPermissions(value = { "ci", "ci:task" }, logical = AND)
-	public RespBase<?> getDependencys(Integer appClusterId, Integer taskId) {
+	public RespBase<?> getDependencys(Integer appClusterId, Integer taskId, Integer tarOrBranch) {
 		Assert.notNull(appClusterId, "appClusterId is null");
 		RespBase<Object> resp = RespBase.create();
-		List<TaskBuildCommand> taskBuildCommands = taskService.getDependency(appClusterId, taskId);
+		List<TaskBuildCommand> taskBuildCommands = taskService.getDependency(appClusterId, taskId, tarOrBranch);
 		resp.forMap().put("list", taskBuildCommands);
 		return resp;
 	}
