c1a064d001f4d104a7a689c1c9775673b5d8b87b
heweijie <babaa1f4@163.com>
2020-04-24 16:19:05 +0800

    fix bug

---
+++ super-devops-erm/super-devops-erm-core/src/main/java/com/wl4g/devops/erm/util/SshkeyUtils.java
@@ -0,0 +1,26 @@
+package com.wl4g.devops.erm.util;
+
+import com.wl4g.devops.tool.common.crypto.CrypticSource;
+import com.wl4g.devops.tool.common.crypto.symmetric.AESCryptor;
+
+import static com.google.common.base.Charsets.UTF_8;
+
+/**
+ * @author vjay
+ * @date 2020-04-24 16:13:00
+ */
+public class SshkeyUtils {
+
+
+    public static String encryptSshkeyToHex(String cipherKey,String sshKey){
+        AESCryptor aes = new AESCryptor();
+        return aes.encrypt(cipherKey.getBytes(UTF_8), new CrypticSource(sshKey)).toHex();
+    }
+
+    public static String decryptSshkeyFromHex(String cipherKey,String hexSshKey){
+        AESCryptor aes = new AESCryptor();
+        return aes.decrypt(cipherKey.getBytes(UTF_8), CrypticSource.fromHex(hexSshKey)).toString();
+    }
+
+
+}
