e5b54acea2b5ef12a88638654e55be30d84db412
heweijie <babaa1f4@163.com>
2020-05-06 11:28:13 +0800

    fix error

--- super-devops-dao/src/main/java/com/wl4g/devops/dao/ci/PipelineHistoryDao.java
+++ super-devops-dao/src/main/java/com/wl4g/devops/dao/ci/PipelineHistoryDao.java
@@ -1,6 +1,10 @@
 package com.wl4g.devops.dao.ci;
 
 import com.wl4g.devops.common.bean.ci.PipelineHistory;
+import com.wl4g.devops.common.bean.ci.TaskHistory;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface PipelineHistoryDao {
     int deleteByPrimaryKey(Integer id);
@@ -14,4 +18,9 @@ public interface PipelineHistoryDao {
     int updateByPrimaryKeySelective(PipelineHistory record);
 
     int updateByPrimaryKey(PipelineHistory record);
+
+    List<TaskHistory> list(@Param("pipeName") String pipeName, @Param("clusterName") String clusterName,
+                           @Param("environment") String environment, @Param("startDate") String startDate,
+                           @Param("endDate") String endDate, @Param("providerKind") String providerKind);
+
 }
