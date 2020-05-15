feb22d6619886e4490bd9d66074e8e1cb5920762
983708408@qq.com <983708408@qq.com>
2020-05-15 17:22:35 +0800

    add cdn cookies

--- super-devops-iam/super-devops-iam-common/src/main/java/com/wl4g/devops/iam/common/web/servlet/IamCookie.java
+++ super-devops-iam/super-devops-iam-common/src/main/java/com/wl4g/devops/iam/common/web/servlet/IamCookie.java
@@ -20,15 +20,35 @@ package com.wl4g.devops.iam.common.web.servlet;
 
 import static java.util.Objects.isNull;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.shiro.web.servlet.Cookie;
 import org.apache.shiro.web.servlet.SimpleCookie;
 
 /**
- * {@link IamCookie}
+ * {@link IamCookie}.
+ * 
+ * <p>
+ * Chrome 80+ plans to make lax the default. At this point, the site can choose
+ * to explicitly turn off the samesite property and set it to none. However, the
+ * premise is that the secure property must be set at the same time (cookies can
+ * only be sent through HTTPS protocol), otherwise it is invalid.
+ * 
+ * <pre>
+ * Invalid setting:
+ * Set-Cookie: MySessionID=abc123; SameSite=None
+ * 
+ * Valid setting:
+ * Set-Cookie: MySessionID=abc123; SameSite=None; Secure
+ * </pre>
+ * </p>
  * 
  * @author Wangl.sir &lt;wanglsir@gmail.com, 983708408@qq.com&gt;
  * @version 2020年5月8日 v1.0.0
- * @see
+ * @see <a href=
+ *      'https://www.zhihu.com/question/373011996?utm_source=qq'>Chrome80+
+ *      SameSite Features</a>.
  */
 public class IamCookie extends SimpleCookie {
 
@@ -42,6 +62,14 @@ public class IamCookie extends SimpleCookie {
 
 	public IamCookie(Cookie cookie) {
 		super(cookie);
+	}
+
+	@Override
+	public void saveTo(HttpServletRequest request, HttpServletResponse response) {
+		if (!isSecure() && request.isSecure()) {
+			setSecure(true);
+		}
+		super.saveTo(request, response);
 	}
 
 	/**
