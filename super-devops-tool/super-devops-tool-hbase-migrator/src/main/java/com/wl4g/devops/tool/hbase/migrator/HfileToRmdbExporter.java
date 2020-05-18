c9208d30c3bd2b8a296a1a4bb92564044cb104b9
983708408@qq.com <983708408@qq.com>
2020-05-18 16:40:24 +0800

    fix hbase1.2.5 -dependto- protobuf-2.5.0

--- super-devops-tool/super-devops-tool-hbase-migrator/src/main/java/com/wl4g/devops/tool/hbase/migrator/HfileToRmdbExporter.java
+++ super-devops-tool/super-devops-tool-hbase-migrator/src/main/java/com/wl4g/devops/tool/hbase/migrator/HfileToRmdbExporter.java
@@ -54,9 +54,10 @@ public class HfileToRmdbExporter {
 	 * 
 	 * <pre>
 	 * yarn jar super-devops-tool-hbase-migrator-master.jar \
-	 * com.wl4g.devops.tool.hbase.migrator.HfileBulkExporter \
+	 * com.wl4g.devops.tool.hbase.migrator.HfileToRmdbExporter \
 	 * -z emr-header-1:2181 \
 	 * -t safeclound.tb_elec_power \
+	 * -d mysql \
 	 * -s 11111112,ELE_R_P,134,01,20180919110850989 \
 	 * -e 11111112,ELE_R_P,134,01,20180921124050540
 	 * </pre>
