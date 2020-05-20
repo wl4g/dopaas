ad7c9d325346f9c7e32b5400efcee42c1e64acf9
heweijie <babaa1f4@163.com>
2020-05-20 11:29:44 +0800

    add

---
+++ super-devops-iam/super-devops-iam-client/src/main/java/com/wl4g/devops/iam/client/utils/IamOrganizationUtils.java
@@ -0,0 +1,16 @@
+package com.wl4g.devops.iam.client.utils;
+
+import com.wl4g.devops.iam.common.subject.IamPrincipalInfo;
+import com.wl4g.devops.iam.common.utils.IamSecurityHolder;
+
+/**
+ * @author vjay
+ * @date 2020-05-20 11:18:00
+ */
+public class IamOrganizationUtils {
+
+    public void getOrganization() {
+        IamPrincipalInfo principalInfo = IamSecurityHolder.getPrincipalInfo();
+    }
+
+}
