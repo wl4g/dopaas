90e3af3e34148a610a09d9fb2befec885ab787ca
983708408@qq.com <983708408@qq.com>
2020-06-17 11:25:20 +0800

    optimizing PeriodFormatter

--- super-devops-components/components-tools/tools-common/src/main/java/com/wl4g/devops/tool/common/lang/period/SamplePeriodFormatter.java
+++ super-devops-components/components-tools/tools-common/src/main/java/com/wl4g/devops/tool/common/lang/period/SamplePeriodFormatter.java
@@ -17,7 +17,6 @@ package com.wl4g.devops.tool.common.lang.period;
 
 import static java.lang.System.currentTimeMillis;
 import static com.wl4g.devops.tool.common.lang.Assert2.isTrue;
-import static java.lang.String.valueOf;
 
 /**
  * {@link SamplePeriodFormatter}
@@ -27,6 +26,19 @@ import static java.lang.String.valueOf;
  * @since
  */
 public class SamplePeriodFormatter extends PeriodFormatterHolder {
+
+	/**
+	 * Always print timespan in decimal format.
+	 */
+	private boolean printDecimalAlway = true;
+
+	public boolean isPrintDecimalAlway() {
+		return printDecimalAlway;
+	}
+
+	public void setPrintDecimalAlway(boolean printDecimalAlway) {
+		this.printDecimalAlway = printDecimalAlway;
+	}
 
 	@Override
 	public String formatHumanDate(long nowTime, long targetTime) {
@@ -38,75 +50,180 @@ public class SamplePeriodFormatter extends PeriodFormatterHolder {
 
 		long sec = (diffSec >= 60 ? diffSec % 60 : diffSec);
 		long min = (diffSec = (diffSec / 60)) >= 60 ? diffSec % 60 : diffSec;
-		long hrs = (diffSec = (diffSec / 60)) >= 24 ? diffSec % 24 : diffSec;
+		long hours = (diffSec = (diffSec / 60)) >= 24 ? diffSec % 24 : diffSec;
 		long days = (diffSec = (diffSec / 24)) >= 30 ? diffSec % 30 : diffSec;
 		long months = (diffSec = (diffSec / 30)) >= 12 ? diffSec % 12 : diffSec;
 		long years = (diffSec = (diffSec / 12));
 
 		if (years > 0) {
 			if (years == 1) {
-				sb.append(" ".concat(getLocalizedMessage("period.formatter.year")));
+				sb.append(" ");
+				sb.append(getLocalizedMessage("period.formatter.year"));
 			} else {
-				sb.append(" ".concat(valueOf(years).concat(" ")).concat(getLocalizedMessage("period.formatter.years")));
-			}
-			if (years <= 6 && months > 0) {
-				if (months == 1) {
-					sb.append(" ".concat(getLocalizedMessage("period.formatter.month")));
+				if (isPrintDecimalAlway()) {
+					sb.append(" ");
+					sb.append(years);
+					if (months > 0) {
+						sb.append(".");
+						sb.append(months);
+					}
+					sb.append(" ");
+					sb.append(getLocalizedMessage("period.formatter.years"));
 				} else {
-					sb.append(" ".concat(valueOf(months)).concat(" ").concat(getLocalizedMessage("period.formatter.months")));
+					sb.append(" ");
+					sb.append(years);
+					sb.append(" ");
+					sb.append(getLocalizedMessage("period.formatter.years"));
+				}
+			}
+			if (!isPrintDecimalAlway()) {
+				if (years <= 6 && months > 0) {
+					if (months == 1) {
+						sb.append(" ");
+						sb.append(getLocalizedMessage("period.formatter.month"));
+					} else {
+						sb.append(" ");
+						sb.append(months);
+						sb.append(" ");
+						sb.append(getLocalizedMessage("period.formatter.months"));
+					}
 				}
 			}
 		} else if (months > 0) {
 			if (months == 1) {
-				sb.append(" ".concat(getLocalizedMessage("period.formatter.month")));
+				sb.append(" ");
+				sb.append(getLocalizedMessage("period.formatter.month"));
 			} else {
-				sb.append(" ".concat(valueOf(months)).concat(" ").concat(getLocalizedMessage("period.formatter.months")));
-			}
-			if (months <= 6 && days > 0) {
-				if (days == 1) {
-					sb.append(" ".concat(getLocalizedMessage("period.formatter.day")));
+				if (isPrintDecimalAlway()) {
+					sb.append(" ");
+					sb.append(months);
+					if (days > 0) {
+						sb.append(".");
+						sb.append(days);
+					}
+					sb.append(" ");
+					sb.append(getLocalizedMessage("period.formatter.months"));
 				} else {
-					sb.append(" ".concat(valueOf(days)).concat(" ").concat(getLocalizedMessage("period.formatter.days")));
+					sb.append(" ");
+					sb.append(months);
+					sb.append(" ");
+					sb.append(getLocalizedMessage("period.formatter.months"));
+				}
+			}
+			if (!isPrintDecimalAlway()) {
+				if (months <= 6 && days > 0) {
+					if (days == 1) {
+						sb.append(" ");
+						sb.append(getLocalizedMessage("period.formatter.day"));
+					} else {
+						sb.append(" ");
+						sb.append(days);
+						sb.append(" ");
+						sb.append(getLocalizedMessage("period.formatter.days"));
+					}
 				}
 			}
 		} else if (days > 0) {
 			if (days == 1) {
-				sb.append(" ".concat(getLocalizedMessage("period.formatter.day")));
+				sb.append(" ");
+				sb.append(getLocalizedMessage("period.formatter.day"));
 			} else {
-				sb.append(" ".concat(valueOf(days).concat(" ")).concat(getLocalizedMessage("period.formatter.days")));
-			}
-			if (days <= 3 && hrs > 0) {
-				if (hrs == 1) {
-					sb.append(" ".concat(getLocalizedMessage("period.formatter.hour")));
+				if (isPrintDecimalAlway()) {
+					sb.append(" ");
+					sb.append(days);
+					if (hours > 0) {
+						sb.append(".");
+						sb.append(hours);
+					}
+					sb.append(" ");
+					sb.append(getLocalizedMessage("period.formatter.days"));
 				} else {
-					sb.append(" ".concat(valueOf(hrs).concat(" ").concat(getLocalizedMessage("period.formatter.hours"))));
+					sb.append(" ");
+					sb.append(days);
+					sb.append(" ");
+					sb.append(getLocalizedMessage("period.formatter.days"));
 				}
 			}
-		} else if (hrs > 0) {
-			if (hrs == 1) {
-				sb.append(" ".concat(getLocalizedMessage("period.formatter.hour")));
-			} else {
-				sb.append(" ".concat(valueOf(hrs).concat(" ")).concat(getLocalizedMessage("period.formatter.hours")));
+			if (!isPrintDecimalAlway()) {
+				if (days <= 3 && hours > 0) {
+					if (hours == 1) {
+						sb.append(" ");
+						sb.append(getLocalizedMessage("period.formatter.hour"));
+					} else {
+						sb.append(" ");
+						sb.append(hours);
+						sb.append(" ");
+						sb.append(getLocalizedMessage("period.formatter.hours"));
+					}
+				}
 			}
-			if (min > 1) {
-				sb.append(" ".concat(valueOf(min).concat(" ")).concat(getLocalizedMessage("period.formatter.minutes")));
+		} else if (hours > 0) {
+			if (hours == 1) {
+				sb.append(" ");
+				sb.append(getLocalizedMessage("period.formatter.hour"));
+			} else {
+				if (isPrintDecimalAlway()) {
+					sb.append(" ");
+					sb.append(hours);
+					if (min > 0) {
+						sb.append(".");
+						sb.append(min);
+					}
+					sb.append(" ");
+					sb.append(getLocalizedMessage("period.formatter.hours"));
+				} else {
+					sb.append(" ");
+					sb.append(hours);
+					sb.append(" ");
+					sb.append(getLocalizedMessage("period.formatter.hours"));
+				}
+			}
+			if (!isPrintDecimalAlway()) {
+				if (min > 1) {
+					sb.append(" ");
+					sb.append(min);
+					sb.append(" ");
+					sb.append(getLocalizedMessage("period.formatter.minutes"));
+				}
 			}
 		} else if (min > 0) {
 			if (min == 1) {
-				sb.append(" ".concat(getLocalizedMessage("period.formatter.minute")));
+				sb.append(" ");
+				sb.append(getLocalizedMessage("period.formatter.minute"));
 			} else {
-				sb.append(" ".concat(valueOf(min)).concat(" ").concat(getLocalizedMessage("period.formatter.minutes")));
+				if (isPrintDecimalAlway()) {
+					sb.append(" ");
+					sb.append(min);
+					if (sec > 0) {
+						sb.append(".");
+						sb.append(sec);
+					}
+					sb.append(" ");
+					sb.append(getLocalizedMessage("period.formatter.minutes"));
+				} else {
+					sb.append(" ");
+					sb.append(min);
+					sb.append(" ");
+					sb.append(getLocalizedMessage("period.formatter.minutes"));
+				}
 			}
 			if (sec > 1) {
-				sb.append(" ".concat(valueOf(sec)).concat(" ").concat(getLocalizedMessage("period.formatter.seconds")));
+				sb.append(" ");
+				sb.append(sec);
+				sb.append(" ");
+				sb.append(getLocalizedMessage("period.formatter.seconds"));
 			}
 		} else {
-			if (sec <= 1) {
-				sb.append(" ".concat(getLocalizedMessage("period.formatter.about")).concat(" ")
-						.concat(getLocalizedMessage("period.formatter.second")));
+			if (sec <= 30) {
+				sb.append(" ");
+				sb.append(getLocalizedMessage("period.formatter.just"));
+				sb.append(" ");
 			} else {
-				sb.append(" ".concat(getLocalizedMessage("period.formatter.about")).concat(" ")
-						.concat(getLocalizedMessage("period.formatter.seconds")));
+				sb.append(" ");
+				sb.append(getLocalizedMessage("period.formatter.about"));
+				sb.append(sec);
+				sb.append(" ");
+				sb.append(getLocalizedMessage("period.formatter.seconds"));
 			}
 		}
 		sb.append(" ".concat(getLocalizedMessage("period.formatter.ago")));
