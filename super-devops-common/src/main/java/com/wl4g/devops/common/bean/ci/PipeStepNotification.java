4f3478d111a4e3247c970f92a6740be013dbe2ed
983708408@qq.com <983708408@qq.com>
2020-07-09 17:00:48 +0800

    optimizing: cleanup

--- super-devops-common/src/main/java/com/wl4g/devops/common/bean/ci/PipeStepNotification.java
+++ super-devops-common/src/main/java/com/wl4g/devops/common/bean/ci/PipeStepNotification.java
@@ -1,66 +1,66 @@
 package com.wl4g.devops.common.bean.ci;
 
 import com.wl4g.devops.common.bean.BaseBean;
-import com.wl4g.devops.tool.common.collection.Collections2;
+import com.wl4g.devops.components.tools.common.collection.Collections2;
 import org.apache.commons.lang3.math.NumberUtils;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class PipeStepNotification extends BaseBean {
-    private static final long serialVersionUID = 6815608076300843748L;
+	private static final long serialVersionUID = 6815608076300843748L;
 
-    private Integer pipeId;
+	private Integer pipeId;
 
-    private Integer enable;
+	private Integer enable;
 
-    private Integer[] contactGroupId;
+	private Integer[] contactGroupId;
 
-    private String contactGroupIds;
+	private String contactGroupIds;
 
-    public Integer getPipeId() {
-        return pipeId;
-    }
+	public Integer getPipeId() {
+		return pipeId;
+	}
 
-    public void setPipeId(Integer pipeId) {
-        this.pipeId = pipeId;
-    }
+	public void setPipeId(Integer pipeId) {
+		this.pipeId = pipeId;
+	}
 
-    public Integer getEnable() {
-        return enable;
-    }
+	public Integer getEnable() {
+		return enable;
+	}
 
-    public void setEnable(Integer enable) {
-        this.enable = enable;
-    }
+	public void setEnable(Integer enable) {
+		this.enable = enable;
+	}
 
-    public Integer[] getContactGroupId() {
-        return contactGroupId;
-    }
+	public Integer[] getContactGroupId() {
+		return contactGroupId;
+	}
 
-    public void setContactGroupId(Integer[] contactGroupId) {
-        this.contactGroupId = contactGroupId;
-    }
+	public void setContactGroupId(Integer[] contactGroupId) {
+		this.contactGroupId = contactGroupId;
+	}
 
-    public void setContactGroupId2(String[] contactGroupId) {
-        if(!Collections2.isEmptyArray(contactGroupId)){
-            List<Integer> list = new ArrayList<>();
-            for(int i = 0; i<contactGroupId.length;i++){
-                if(NumberUtils.isCreatable(contactGroupId[i])){
-                    list.add(Integer.parseInt(contactGroupId[i]));
-                }
-            }
-            Integer[] result = new Integer[list.size()];
-            list.toArray(result);
-            this.contactGroupId = result;
-        }
-    }
+	public void setContactGroupId2(String[] contactGroupId) {
+		if (!Collections2.isEmptyArray(contactGroupId)) {
+			List<Integer> list = new ArrayList<>();
+			for (int i = 0; i < contactGroupId.length; i++) {
+				if (NumberUtils.isCreatable(contactGroupId[i])) {
+					list.add(Integer.parseInt(contactGroupId[i]));
+				}
+			}
+			Integer[] result = new Integer[list.size()];
+			list.toArray(result);
+			this.contactGroupId = result;
+		}
+	}
 
-    public String getContactGroupIds() {
-        return contactGroupIds;
-    }
+	public String getContactGroupIds() {
+		return contactGroupIds;
+	}
 
-    public void setContactGroupIds(String contactGroupIds) {
-        this.contactGroupIds = contactGroupIds;
-    }
+	public void setContactGroupIds(String contactGroupIds) {
+		this.contactGroupIds = contactGroupIds;
+	}
 }
