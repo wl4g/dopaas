96b6179b5dc391bcf9d328c916db583dec7840cd
heweijie <babaa1f4@163.com>
2020-07-21 14:46:11 +0800

    gateway

--- super-devops-gateway/super-devops-gateway-server/src/main/java/com/wl4g/devops/gataway/server/route/IRouteAlterSubscriber.java
+++ super-devops-gateway/super-devops-gateway-server/src/main/java/com/wl4g/devops/gataway/server/route/IRouteAlterSubscriber.java
@@ -17,9 +17,10 @@ package com.wl4g.devops.gataway.server.route;
 
 public interface IRouteAlterSubscriber {
 
-	/**
-	 * 初始化订阅者，用来初始化 接收持久化刷新的消息
-	 */
-	void initSubscriber();
+
+    /**
+     * 初始化订阅者，用来初始化 接收持久化刷新的消息
+     */
+    //void initSubscriber();
 
 }
--- super-devops-gateway/super-devops-gateway-server/src/main/java/com/wl4g/devops/gataway/server/route/PeriodicityRefresh.java
+++ super-devops-gateway/super-devops-gateway-server/src/main/java/com/wl4g/devops/gataway/server/route/PeriodicityRefresh.java
@@ -3,7 +3,6 @@ package com.wl4g.devops.gataway.server.route;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 /**
@@ -17,10 +16,13 @@ public class PeriodicityRefresh {
 	@Autowired
 	private ApplicationContext applicationContext;
 
-	// @Scheduled(cron="0/5 * * * * ? ")
-	@Scheduled(fixedRate = 5000, initialDelay = 10000)
-	public void reportCurrentTime() {
-		applicationContext.getBean(IRouteCacheRefresh.class).flushRoutesPermanentToMemery();
-	}
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    //@Scheduled(cron="0/5 * * * * ? ")
+    //@Scheduled(fixedRate = 5000, initialDelay = 10000)
+    public void reportCurrentTime() {
+        applicationContext.getBean(IRouteCacheRefresh.class).flushRoutesPermanentToMemery();
+    }
 
 }
