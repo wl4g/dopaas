e3180189fb9b5fbcea074fd615b52b8531b91680
heweijie <babaa1f4@163.com>
2020-09-17 15:30:28 +0800

    fix: bug

--- xcloud-devops-dts/xcloud-devops-dts-codegen/src/main/java/com/wl4g/devops/dts/codegen/engine/AbstractGeneratorProvider.java
+++ xcloud-devops-dts/xcloud-devops-dts-codegen/src/main/java/com/wl4g/devops/dts/codegen/engine/AbstractGeneratorProvider.java
@@ -15,12 +15,6 @@
  */
 package com.wl4g.devops.dts.codegen.engine;
 
-import static com.wl4g.components.core.utils.expression.SpelExpressions.create;
-import static com.wl4g.components.common.collection.Collections2.ensureMap;
-import static com.wl4g.components.common.io.ByteStreamUtils.readFullyToString;
-import static com.wl4g.components.common.io.FileIOUtils.writeFile;
-import static com.wl4g.components.common.view.Freemarkers.renderingTemplateToString;
-
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.wl4g.components.common.annotation.Nullable;
 import com.wl4g.components.common.log.SmartLogger;
@@ -34,22 +28,26 @@ import com.wl4g.devops.dts.codegen.engine.naming.CSharpSpecs;
 import com.wl4g.devops.dts.codegen.engine.naming.GolangSpecs;
 import com.wl4g.devops.dts.codegen.engine.naming.JavaSpecs;
 import com.wl4g.devops.dts.codegen.engine.naming.PythonSpecs;
-import static com.wl4g.devops.dts.codegen.utils.FreemarkerUtils.defaultGenConfigurer;
 import freemarker.template.Template;
 
-import static java.lang.String.valueOf;
-import static java.util.Objects.isNull;
-import java.io.*;
+import javax.validation.constraints.NotBlank;
+import java.io.File;
+import java.io.IOException;
 import java.util.*;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-
+import static com.wl4g.components.common.collection.Collections2.ensureMap;
+import static com.wl4g.components.common.io.ByteStreamUtils.readFullyToString;
+import static com.wl4g.components.common.io.FileIOUtils.writeFile;
 import static com.wl4g.components.common.lang.Assert2.hasTextOf;
 import static com.wl4g.components.common.lang.Assert2.notNullOf;
 import static com.wl4g.components.common.log.SmartLoggerFactory.getLogger;
 import static com.wl4g.components.common.serialize.JacksonUtils.parseJSON;
 import static com.wl4g.components.common.serialize.JacksonUtils.toJSONString;
+import static com.wl4g.components.common.view.Freemarkers.renderingTemplateToString;
+import static com.wl4g.components.core.utils.expression.SpelExpressions.create;
+import static com.wl4g.devops.dts.codegen.utils.FreemarkerUtils.defaultGenConfigurer;
+import static java.lang.String.valueOf;
+import static java.util.Objects.isNull;
 
 /**
  * {@link AbstractGeneratorProvider}
@@ -60,256 +58,265 @@ import static com.wl4g.components.common.serialize.JacksonUtils.toJSONString;
  */
 public abstract class AbstractGeneratorProvider implements GeneratorProvider {
 
-	protected final SmartLogger log = getLogger(getClass());
+    protected final SmartLogger log = getLogger(getClass());
 
-	/**
-	 * {@link GenerateContext}
-	 */
-	protected final GenerateContext context;
+    /**
+     * {@link GenerateContext}
+     */
+    protected final GenerateContext context;
 
-	public AbstractGeneratorProvider(GenerateContext context) {
-		this.context = notNullOf(context, "context");
-	}
+    public AbstractGeneratorProvider(GenerateContext context) {
+        this.context = notNullOf(context, "context");
+    }
 
-	@Override
-	public void run() {
-		try {
-			generate();
-		} catch (Exception e) {
-			throw new IllegalStateException(e);
-		}
-	}
+    @Override
+    public void run() {
+        try {
+            generate();
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
 
-	/**
-	 * Hanlding generation.
-	 * 
-	 * @throws Exception
-	 */
-	protected abstract void generate() throws Exception;
+    /**
+     * Hanlding generation.
+     *
+     * @throws Exception
+     */
+    protected abstract void generate() throws Exception;
 
-	protected void doGenerate(String provider) throws Exception {
-		GenProject project = context.getGenProject();
+    protected void doGenerate(String provider) throws Exception {
+        GenProject project = context.getGenProject();
 
-		// Load templates.
-		List<TemplateWrapper> tpls = loadTemplates(provider);
+        // Load templates.
+        List<TemplateWrapper> tpls = loadTemplates(provider);
 
-		// Handling generate
-		doHandleGenerateAndSave(tpls, project, context.getJobDir().getAbsolutePath());
-	}
+        // Handling generate
+        doHandleGenerateAndSave(tpls, project, context.getJobDir().getAbsolutePath());
+    }
 
-	/**
-	 * Do handling rendering generate and save.
-	 * 
-	 * @param tpls
-	 * @param project
-	 * @param targetBasePath
-	 * @throws Exception
-	 */
-	private void doHandleGenerateAndSave(List<TemplateWrapper> tpls, GenProject project, String targetBasePath) throws Exception {
-		for (TemplateWrapper tpl : tpls) {
-			// Create rednering model.
-			Map<String, Object> model = createRenderingModel(tpl.getTplPath(), project, null);
-			if (tpl.isTpl()) {
-				// foreach template by table
-				if (tpl.isForeachTpl()) {
-					for (GenTable tab : project.getGenTables()) {
-						// Additidtion table model attributes
-						model.putAll(toRenderingModel(tab));
-						// Rendering tpl
-						String targetPath = targetBasePath.concat("/").concat(resolveExpressionPath(tpl.getTplPath(), model));
-						Template template = new Template(tpl.getFileName(), tpl.getFileContent(), defaultGenConfigurer);
-						String fileContent = renderingTemplateToString(template, model);
-						writeFile(new File(targetPath), fileContent, false);
-					}
-				}
-				// Simple template.
-				else {
-					String targetPath = targetBasePath.concat("/").concat(resolveExpressionPath(tpl.getTplPath(), model));
-					// Rendering tpl
-					Template template = new Template(tpl.getFileName(), tpl.getFileContent(), defaultGenConfigurer);
-					String fileContent = renderingTemplateToString(template, model);
-					writeFile(new File(targetPath), fileContent, false);
-				}
-			}
-			// e.g: static file
-			else {
-				String targetPath = targetBasePath + "/" + resolveExpressionPath(tpl.getTplPath(), model);
-				writeFile(new File(targetPath), tpl.getFileContent(), false);
-			}
-		}
-	}
+    /**
+     * Do handling rendering generate and save.
+     *
+     * @param tpls
+     * @param project
+     * @param targetBasePath
+     * @throws Exception
+     */
+    private void doHandleGenerateAndSave(List<TemplateWrapper> tpls, GenProject project, String targetBasePath) throws Exception {
+        for (TemplateWrapper tpl : tpls) {
+            // Create rednering model.
+            Map<String, Object> model = createRenderingModel(tpl.getTplPath(), project);
+            if (tpl.isTpl()) {
+                // foreach template by table
+                if (tpl.isForeachTpl()) {
+                    for (GenTable tab : project.getGenTables()) {
+                        // Additidtion table model attributes
+                        model.putAll(toRenderingModel(tab));
+                        // Rendering tpl
+                        String targetPath = targetBasePath.concat("/").concat(resolveExpressionPath(tpl.getTplPath(), model));
+                        Template template = new Template(tpl.getFileName(), tpl.getFileContent(), defaultGenConfigurer);
+                        String fileContent = renderingTemplateToString(template, model);
+                        writeFile(new File(targetPath), fileContent, false);
+                    }
+                }
+                // Simple template.
+                else {
+                    String targetPath = targetBasePath.concat("/").concat(resolveExpressionPath(tpl.getTplPath(), model));
+                    // Rendering tpl
+                    Template template = new Template(tpl.getFileName(), tpl.getFileContent(), defaultGenConfigurer);
+                    String fileContent = renderingTemplateToString(template, model);
+                    writeFile(new File(targetPath), fileContent, false);
+                }
+            }
+            // e.g: static file
+            else {
+                String targetPath = targetBasePath + "/" + resolveExpressionPath(tpl.getTplPath(), model);
+                writeFile(new File(targetPath), tpl.getFileContent(), false);
+            }
+        }
+    }
 
-	/**
-	 * Customize rendering model
-	 * 
-	 * @param tplPath
-	 * @param project
-	 * @param table
-	 * @return
-	 */
-	protected Map<String, Object> customizeRenderingModel(@NotBlank String tplPath, @NotNull GenProject project,
-			@Nullable GenTable table) {
-		return null;
-	}
+    /**
+     * Customize rendering model
+     *
+     * @param tplPath
+     * @param project
+     * @param table
+     * @return
+     */
+    protected Map<String, Object> customizeRenderingModel(@NotBlank String tplPath,
+                                                          @Nullable Object... beans) {
+        return null;
+    }
 
-	/**
-	 * Create rendering model
-	 * 
-	 * @param tplPath
-	 * @param project
-	 * @param table
-	 * @return
-	 * @throws Exception
-	 */
-	private Map<String, Object> createRenderingModel(String tplPath, GenProject project, GenTable table) throws Exception {
-		// Gets customize model.
-		Map<String, Object> model = ensureMap(customizeRenderingModel(tplPath, project, table));
+    /**
+     * Create rendering model
+     *
+     * @param tplPath
+     * @param project
+     * @param table
+     * @return
+     * @throws Exception
+     */
+    private Map<String, Object> createRenderingModel(String tplPath, Object... beans) throws Exception {
+        // Gets customize model.
+        Map<String, Object> model = ensureMap(customizeRenderingModel(tplPath, beans));
 
-		// Fill requires rendering parameters.
-		model.putAll(toRenderingModel(project));
-		model.putAll(toRenderingModel(table));
+        // Fill requires rendering parameters.
+        if (!isNull(beans)) {
+            for (Object bean : beans) {
+                model.putAll(toRenderingModel(bean));
+            }
+        }
 
-		return model;
-	}
+        return model;
+    }
 
-	/**
-	 * Resolving path SPEL expression
-	 * 
-	 * @param tplPath
-	 * @param model
-	 * @return
-	 */
-	private String resolveExpressionPath(String tplPath, Map<String, Object> model) {
-		if (tplPath.endsWith(".ftl")) {
-			tplPath = tplPath.substring(0, tplPath.length() - 4);
-		}
-		return valueOf(defaultExpressions.resolve(tplPath, model));
-	}
+    /**
+     * Resolving path SPEL expression
+     *
+     * @param tplPath
+     * @param model
+     * @return
+     */
+    private String resolveExpressionPath(String tplPath, Map<String, Object> model) {
+        if (tplPath.endsWith(".ftl")) {
+            tplPath = tplPath.substring(0, tplPath.length() - 4);
+        }
+        return valueOf(defaultExpressions.resolve(tplPath, model));
+    }
 
-	/**
-	 * Gets object to map model
-	 * 
-	 * @param object
-	 * @return
-	 * @throws Exception
-	 */
-	protected static Map<String, Object> toRenderingModel(Object object) throws Exception {
-		String modelJson = toJSONString(object);
-		return parseJSON(modelJson, new TypeReference<HashMap<String, Object>>() {
-		});
-	}
+    /**
+     * Gets object to map model
+     *
+     * @param object
+     * @return
+     * @throws Exception
+     */
+    protected static Map<String, Object> toRenderingModel(Object object) throws Exception {
+        String modelJson = toJSONString(object);
+        return parseJSON(modelJson, new TypeReference<HashMap<String, Object>>() {
+        });
+    }
 
-	/**
-	 * Load {@link Template} list by provider.
-	 * 
-	 * @param provider
-	 * @return
-	 * @throws IOException
-	 */
-	private static List<TemplateWrapper> loadTemplates(String provider) throws IOException {
-		List<TemplateWrapper> tpls = templatesCache.get(provider);
-		if (isNull(tpls)) {
-			synchronized (AbstractGeneratorProvider.class) {
-				tpls = templatesCache.get(provider);
-				if (isNull(tpls)) {
-					tpls = new ArrayList<>();
-					Set<StreamResource> resources = defaultResourceResolver
-							.getResources("classpath:/" + DEFAULT_TPL_BASEPATH + "/" + provider + "/**/*.ftl");
-					for (StreamResource res : resources) {
-						if (res.getFile().isFile()) {
-							tpls.add(wrapTemplate(res, provider));
-						}
-					}
-					templatesCache.put(provider, tpls);
-				}
-			}
-		}
-		return tpls;
-	}
+    /**
+     * Load {@link Template} list by provider.
+     *
+     * @param provider
+     * @return
+     * @throws IOException
+     */
+    private static List<TemplateWrapper> loadTemplates(String provider) throws IOException {
+        List<TemplateWrapper> tpls = templatesCache.get(provider);
+        if (isNull(tpls)) {
+            synchronized (AbstractGeneratorProvider.class) {
+                tpls = templatesCache.get(provider);
+                if (isNull(tpls)) {
+                    tpls = new ArrayList<>();
+                    Set<StreamResource> resources = defaultResourceResolver
+                            .getResources("classpath:/" + DEFAULT_TPL_BASEPATH + "/" + provider + "/**/*.ftl");
+                    for (StreamResource res : resources) {
+                        if (res.getFile().isFile()) {
+                            tpls.add(wrapTemplate(res, provider));
+                        }
+                    }
+                    templatesCache.put(provider, tpls);
+                }
+            }
+        }
+        return tpls;
+    }
 
-	/**
-	 * Wrapper {@link Template}
-	 * 
-	 * @param res
-	 * @param provider
-	 * @return
-	 * @throws IOException
-	 */
-	private static TemplateWrapper wrapTemplate(StreamResource res, String provider) throws IOException {
-		String path = res.getURI().getPath();
-		String splitStr = DEFAULT_TPL_BASEPATH + "/" + provider + "/";
-		int i = path.indexOf(splitStr);
-		if (i >= 0) {
-			path = path.substring(i + splitStr.length());
-		}
-		return new TemplateWrapper(path, res.getFilename(), readFullyToString(res.getInputStream()));
-	}
+    /**
+     * Wrapper {@link Template}
+     *
+     * @param res
+     * @param provider
+     * @return
+     * @throws IOException
+     */
+    private static TemplateWrapper wrapTemplate(StreamResource res, String provider) throws IOException {
+        String path = res.getURI().getPath();
+        String splitStr = DEFAULT_TPL_BASEPATH + "/" + provider + "/";
+        int i = path.indexOf(splitStr);
+        if (i >= 0) {
+            path = path.substring(i + splitStr.length());
+        }
+        return new TemplateWrapper(path, res.getFilename(), readFullyToString(res.getInputStream()));
+    }
 
-	/**
-	 * {@link TemplateWrapper}
-	 *
-	 * @author Wangl.sir <wanglsir@gmail.com, 983708408@qq.com>
-	 * @author Vjay
-	 * @version v1.0 2020-09-16
-	 * @since
-	 */
-	protected final static class TemplateWrapper {
+    /**
+     * {@link TemplateWrapper}
+     *
+     * @author Wangl.sir <wanglsir@gmail.com, 983708408@qq.com>
+     * @author Vjay
+     * @version v1.0 2020-09-16
+     * @since
+     */
+    protected final static class TemplateWrapper {
 
-		// Template class path.
-		private final String tplPath;
-		private final String fileName;
-		private final String fileContent;
-		private final boolean isTpl;
-		private final boolean isForeachTpl;
+        // Template class path.
+        private final String tplPath;
+        private final String fileName;
+        private final String fileContent;
+        private final boolean isTpl;
+        private final boolean isForeachTpl;
 
-		protected TemplateWrapper(String tplPath, String filename, String fileContent) {
-			hasTextOf(tplPath, "tplClassPath");
-			hasTextOf(filename, "filename");
-			hasTextOf(fileContent, "fileContent");
-			this.tplPath = tplPath;
-			this.fileName = filename;
-			this.fileContent = fileContent;
-			this.isTpl = filename.endsWith(DEFAULT_TPL_SUFFIX);
-			this.isForeachTpl = filename.contains(VARIABLE_ENTITY_NAME);
-		}
+        protected TemplateWrapper(String tplPath, String filename, String fileContent) {
+            hasTextOf(tplPath, "tplClassPath");
+            hasTextOf(filename, "filename");
+            hasTextOf(fileContent, "fileContent");
+            this.tplPath = tplPath;
+            this.fileName = filename;
+            this.fileContent = fileContent;
+            this.isTpl = filename.endsWith(DEFAULT_TPL_SUFFIX);
+            this.isForeachTpl = filename.contains(VARIABLE_ENTITY_NAME);
+        }
 
-		public String getTplPath() {
-			return tplPath;
-		}
+        public String getTplPath() {
+            return tplPath;
+        }
 
-		public String getFileName() {
-			return fileName;
-		}
+        public String getFileName() {
+            return fileName;
+        }
 
-		public String getFileContent() {
-			return fileContent;
-		}
+        public String getFileContent() {
+            return fileContent;
+        }
 
-		public boolean isTpl() {
-			return isTpl;
-		}
+        public boolean isTpl() {
+            return isTpl;
+        }
 
-		public boolean isForeachTpl() {
-			return isForeachTpl;
-		}
+        public boolean isForeachTpl() {
+            return isForeachTpl;
+        }
 
-	}
+    }
 
-	// Template configuration.
-	private static final String DEFAULT_TPL_BASEPATH = "projects-template";
-	private static final String DEFAULT_TPL_SUFFIX = ".tpl";
+    // Template configuration.
+    private static final String DEFAULT_TPL_BASEPATH = "projects-template";
+    private static final String DEFAULT_TPL_SUFFIX = ".tpl";
 
-	// Template configuration.
-	private static final String VARIABLE_ENTITY_NAME = "entityName";
+    // Template configuration.
+    private static final String VARIABLE_ENTITY_NAME = "entityName";
 
-	/** Global project {@link Template} cache. */
-	private static final Map<String, List<TemplateWrapper>> templatesCache = new HashMap<>();
+    /**
+     * Global project {@link Template} cache.
+     */
+    private static final Map<String, List<TemplateWrapper>> templatesCache = new HashMap<>();
 
-	/** {@link SpelExpressions} */
-	private static final SpelExpressions defaultExpressions = create(CSharpSpecs.class, GolangSpecs.class, JavaSpecs.class,
-			PythonSpecs.class);
+    /**
+     * {@link SpelExpressions}
+     */
+    private static final SpelExpressions defaultExpressions = create(CSharpSpecs.class, GolangSpecs.class, JavaSpecs.class,
+            PythonSpecs.class);
 
-	/** {@link ClassPathResourcePatternResolver} */
-	private static final ClassPathResourcePatternResolver defaultResourceResolver = new ClassPathResourcePatternResolver();
+    /**
+     * {@link ClassPathResourcePatternResolver}
+     */
+    private static final ClassPathResourcePatternResolver defaultResourceResolver = new ClassPathResourcePatternResolver();
 
 }
