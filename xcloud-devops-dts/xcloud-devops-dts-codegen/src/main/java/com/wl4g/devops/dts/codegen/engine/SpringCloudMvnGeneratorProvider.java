968dff42c11edbc3bbfc0d13477a4b23afaa0a93
983708408@qq.com <983708408@qq.com>
2020-09-18 15:36:56 +0800

    fix: optimizing final0

--- xcloud-devops-dts/xcloud-devops-dts-codegen/src/main/java/com/wl4g/devops/dts/codegen/engine/SpringCloudMvnGeneratorProvider.java
+++ xcloud-devops-dts/xcloud-devops-dts-codegen/src/main/java/com/wl4g/devops/dts/codegen/engine/SpringCloudMvnGeneratorProvider.java
@@ -16,7 +16,6 @@
 package com.wl4g.devops.dts.codegen.engine;
 
 import com.wl4g.devops.dts.codegen.core.context.GenerateContext;
-import com.wl4g.devops.dts.codegen.engine.converter.DbTypeConverter.CodeLanguage;
 
 /**
  * SpringMVC service, serviceImpl and controller generator.
@@ -29,11 +28,6 @@ public class SpringCloudMvnGeneratorProvider extends AbstractGeneratorProvider {
 
 	public SpringCloudMvnGeneratorProvider(GenerateContext context) {
 		super(context);
-	}
-
-	@Override
-	public CodeLanguage getLanguage() {
-		return CodeLanguage.JAVA;
 	}
 
 	@Override
