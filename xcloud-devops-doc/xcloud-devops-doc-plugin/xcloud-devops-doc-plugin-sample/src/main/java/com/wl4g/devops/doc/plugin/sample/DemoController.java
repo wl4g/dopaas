80edaf07e1aac06552d0e894755d0a243a61ad98
wanglsir <wangl@shangmaikeji.com>
2020-12-08 18:50:55 +0800

    feat: add supports maven-plugin of springfox

---
+++ xcloud-devops-doc/xcloud-devops-doc-plugin/xcloud-devops-doc-plugin-sample/src/main/java/com/wl4g/devops/doc/plugin/sample/DemoController.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2017 ~ 2025 the original author or authors. <wanglsir@gmail.com, 983708408@qq.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.wl4g.devops.doc.plugin.sample;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+/**
+ * {@link DemoController}
+ * 
+ * @author Wangl.sir &lt;wanglsir@gmail.com, 983708408@qq.com&gt;
+ * @version v1.0 2020-12-08
+ * @sine v1.0
+ * @see
+ */
+@RestController
+@RequestMapping("/demo")
+@Api
+public class DemoController {
+
+	@ApiOperation("Obtain user info")
+	@GetMapping("getUserInfo")
+	public String getUserInfo() {
+		return "jack";
+	}
+
+}
