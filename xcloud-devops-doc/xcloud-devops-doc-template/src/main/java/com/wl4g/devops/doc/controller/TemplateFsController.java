048fec2f790be435b210c782f999ebe556c235e4
heweijie <babaa1f4@163.com>
2020-12-22 11:35:47 +0800

    feat: add file template manager

---
+++ xcloud-devops-doc/xcloud-devops-doc-template/src/main/java/com/wl4g/devops/doc/controller/TemplateFsController.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2017 ~ 2050 the original author or authors <Wanglsir@gmail.com, 983708408@qq.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.wl4g.devops.doc.controller;
+
+import com.wl4g.component.common.web.rest.RespBase;
+import com.wl4g.devops.doc.service.FsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/fs")
+public class TemplateFsController {
+
+	@Autowired
+	private FsService fsService;
+
+	@RequestMapping("getFilesByParent")
+	RespBase<?> getFilesByParent(String parentPath){
+		RespBase<Object> resp = RespBase.create();
+		resp.setData(fsService.getFilesByParent(parentPath));
+		return resp;
+	}
+
+	@RequestMapping("getFileInfo")
+	RespBase<?> getFileInfo(String path) throws IOException{
+		RespBase<Object> resp = RespBase.create();
+		resp.setData(fsService.getFileInfo(path));
+		return resp;
+	}
+
+	@RequestMapping("delFile")
+	RespBase<?> delFile(String path) throws IOException{
+		RespBase<Object> resp = RespBase.create();
+		fsService.delFile(path);
+		return resp;
+	}
+
+	@RequestMapping("addFile")
+	RespBase<?> addFile(String path) throws IOException{
+		RespBase<Object> resp = RespBase.create();
+		fsService.addFile(path);
+		return resp;
+	}
+
+	@RequestMapping("saveFile")
+	RespBase<?> saveFile(String path, String content){
+		RespBase<Object> resp = RespBase.create();
+		fsService.saveFile(path, content);
+		return resp;
+	}
+
+
+	@PostMapping(value = "/uploadFile")
+	public RespBase<?> uploadFile(@RequestParam(value = "file") MultipartFile file) {
+		RespBase<Object> resp = RespBase.create();
+		String fileUrl = fsService.uploadFile(file);
+		resp.setData(fileUrl);
+		return resp;
+	}
+
+	@RequestMapping(value = "/downloadFile/{date}/{fileName:.+}")
+	public ResponseEntity<FileSystemResource> downloadFile(@PathVariable String date, @PathVariable String fileName)
+			throws IOException {
+		return fsService.downloadFile("/" + date + "/" + fileName);
+	}
+
+}
---
+++ xcloud-devops-doc/xcloud-devops-doc-template/src/main/java/com/wl4g/devops/doc/service/impl/FsServiceImpl.java
@@ -0,0 +1,175 @@
+/*
+ * Copyright 2017 ~ 2050 the original author or authors <Wanglsir@gmail.com, 983708408@qq.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.wl4g.devops.doc.service.impl;
+
+import com.wl4g.component.common.io.FileIOUtils;
+import com.wl4g.component.common.lang.DateUtils2;
+import com.wl4g.devops.doc.bean.FileInfo;
+import com.wl4g.devops.doc.config.FsProperties;
+import com.wl4g.devops.doc.service.FsService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class FsServiceImpl implements FsService {
+
+    @Autowired
+    private FsProperties fsProperties;
+
+    @Override
+    public List<FileInfo> getFilesByParent(String parentPath) {
+
+        File file;
+        if (StringUtils.isNotBlank(parentPath)) {
+            file = new File(fsProperties.getBasePath() + parentPath);
+        } else {
+            file = new File(fsProperties.getBasePath());
+        }
+
+        File[] files = file.listFiles();
+        if (files == null) {
+            return null;
+        }
+        List<FileInfo> fileInfos = new ArrayList<>();
+        for (File f : files) {
+            FileInfo fileInfo = new FileInfo();
+            fileInfo.setPath(getRelativePath(f.getAbsolutePath()));
+            fileInfo.setFileName(f.getName());
+            fileInfo.setDir(f.isDirectory());
+            fileInfos.add(fileInfo);
+        }
+        return fileInfos;
+    }
+
+    @Override
+    public FileInfo getFileInfo(String path) throws IOException {
+        File file = new File(fsProperties.getBasePath() + path);
+        FileInfo fileInfo = new FileInfo();
+        fileInfo.setPath(getRelativePath(file.getAbsolutePath()));
+        fileInfo.setFileName(file.getName());
+        fileInfo.setDir(file.isDirectory());
+        if (file.isFile()) {
+            fileInfo.setContent(FileIOUtils.readFileToString(file, "UTF-8"));
+        }
+        return fileInfo;
+    }
+
+    @Override
+    public void delFile(String path) throws IOException {
+        FileIOUtils.deleteDirectory(new File(fsProperties.getBasePath() + path));
+    }
+
+    @Override
+    public void addFile(String path) throws IOException {
+        File file = new File(fsProperties.getBasePath() + path);
+        boolean newFile = new File(path).createNewFile();
+        if (!newFile || !file.exists()) {
+            throw new IOException("create new file fail");
+        }
+    }
+
+    @Override
+    public void saveFile(String path, String content) {
+        File file = new File(fsProperties.getBasePath() + path);
+        if (!file.exists()) {
+            return;
+        }
+        FileIOUtils.writeFile(file, content, false);
+    }
+
+    @Override
+    public String uploadFile(MultipartFile file) {
+        Date now = new Date();
+        String fileName = file.getOriginalFilename();// 文件名
+        String suffixName = fileName.substring(fileName.lastIndexOf("."));// 后缀名
+        fileName = UUID.randomUUID() + suffixName;// 新文件名
+        fileName = "/" + DateUtils2.formatDate(now, "yyyyMMddHHmmss") + "/" + fileName;// 加一级日期目录
+        String path = fsProperties.getBaseFilePath() + fileName;
+        saveFile(file, path);
+
+        //TODO
+        return fileName;
+    }
+
+    /**
+     * @param path 相对路径
+     * @return
+     * @throws IOException
+     */
+    @Override
+    public ResponseEntity<FileSystemResource> downloadFile(String path) throws IOException {
+        File file = new File(fsProperties.getBaseFilePath() + "/" + path);
+        return downloadFile(file);
+    }
+
+    private void saveFile(MultipartFile file, String localPath) {
+        Assert.notNull(file, "文件为空");
+        File dest = new File(localPath);
+        if (!dest.getParentFile().exists()) {
+            dest.getParentFile().mkdirs();
+        }
+        try {
+            file.transferTo(dest);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private ResponseEntity<FileSystemResource> downloadFile(File file) {
+        if (file == null) {
+            return null;
+        }
+        if (!file.exists()) {
+            return null;
+        }
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        String suffixName = file.getName().substring(file.getName().lastIndexOf("."));// 后缀名
+        headers.add("Content-Disposition", "attachment; filename=" + System.currentTimeMillis() + suffixName);
+        headers.add("Pragma", "no-cache");
+        headers.add("Expires", "0");
+        headers.add("Last-Modified", new Date().toString());
+        headers.add("ETag", String.valueOf(System.currentTimeMillis()));
+
+        return ResponseEntity.ok().headers(headers).contentLength(file.length())
+                .contentType(MediaType.parseMediaType("application/octet-stream")).body(new FileSystemResource(file));
+    }
+
+
+    private String getRelativePath(String absolutePath) {
+        String baseFilePath = fsProperties.getBaseFilePath();
+        if (absolutePath.startsWith(baseFilePath)) {
+            return absolutePath.substring(baseFilePath.length());
+        } else {
+            return absolutePath;
+        }
+    }
+
+}
