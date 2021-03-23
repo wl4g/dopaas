f5d987a899cbb30f49ac6f5448a98999c5ed8478
wanglsir <wanglsir@gmail.com>
2021-03-23 11:56:27 +0800

    feat: add xcloud-dopaas-home module

---
+++ xcloud-dopaas-home/xcloud-dopaas-home-service-starter-manager/src/main/java/com/wl4g/dopaas/home/web/OverviewController.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2017 ~ 2050 the original author or authors <Wanglsir@gmail.com, 983708408@qq.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.wl4g.dopaas.home.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wl4g.component.core.web.BaseController;
+
+/**
+ * {@link OverviewController}
+ * 
+ * @author Wangl.sir &lt;wanglsir@gmail.com, 983708408@qq.com&gt;
+ * @version v1.0 2021-03-23
+ * @sine v1.0
+ * @see
+ */
+@RestController
+@RequestMapping("/overview")
+public class OverviewController extends BaseController {
+
+}
