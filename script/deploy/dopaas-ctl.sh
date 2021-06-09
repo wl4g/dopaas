48201cdb5fb7db3621343294946dfe991fb84259
wanglsir <wanglsir@gmail.com>
2021-06-09 18:13:21 +0800

    fix: dopaas-ctl.sh add zookeeper

--- script/deploy/dopaas-ctl.sh
+++ script/deploy/dopaas-ctl.sh
@@ -33,8 +33,9 @@ log " Installation logs writing: $logFile"
 log " -----------------------------------------------------------------------"
 log ""
 
-function doCommandAll() {
-  local cmd=$1
+function doCommandApps() {
+  local cmd=$1 # Requires
+  local targetAppName=$2 # Optional
   [[ -z "$cmd" ]] && logErr "Do ctl command is requires!" && exit -1
 
   local deployBuildModulesSize=0
@@ -53,6 +54,11 @@ function doCommandAll() {
       local buildModule=${deployBuildModules[i]}
       local appName=$(echo "$buildModule"|awk -F ',' '{print $1}')
       local appPort=$(echo "$buildModule"|awk -F ',' '{print $2}')
+      # Matching target appName. (if necessary)
+      if [[ -n "$targetAppName" && "$targetAppName" != "$appName" ]]; then
+        continue
+      fi
+      # Exec remote commands to nodes of appName.
       {
         local count=-1
         for node in `cat $deployClusterNodesConfigPath`; do
@@ -76,25 +82,94 @@ function doCommandAll() {
         done
       } &
     done
-    [ "$asyncDeploy" == "true" ] && wait
   fi
 }
 
+function doCommandZookeeper() {
+  local cmd=$1
+  [[ -z "$cmd" ]] && logErr "Do ctl command is requires!" && exit -1
+
+  local count=-1
+  for node in `cat $deployClusterNodesConfigPath`; do
+    ((count+=1))
+    if [[ $count == 0 || -n "$(echo $node|grep -E '^#')" ]]; then
+      continue # Skip head or annotation rows.
+    fi
+    # Extract node info & trim
+    local host=$(echo $node|awk -F ',' '{print $1}'|sed -e 's/^\s*//' -e 's/\s*$//')
+    local user=$(echo $node|awk -F ',' '{print $2}'|sed -e 's/^\s*//' -e 's/\s*$//')
+    local passwd=$(echo $node|awk -F ',' '{print $3}'|sed -e 's/^\s*//' -e 's/\s*$//')
+    if [[ "$host" == "" || "$user" == "" ]]; then
+      logErr "[zookeeper/$host] Invalid cluster node info, host/user is required! host: $host, user: $user, password: $passwd"; exit -1
+    fi
+    log "[zookeeper/$host] Manage ($cmd) on $host ..." 
+    if [ "$asyncDeploy" == "true" ]; then
+      doRemoteCmd "$user" "$passwd" "$host" "sudo ${zkHome}/bin/zkServer.sh ${cmd}" "false" "true" &
+    else
+      doRemoteCmd "$user" "$passwd" "$host" "sudo ${zkHome}/bin/zkServer.sh ${cmd}" "false" "true"
+    fi
+  done
+}
+
+function waitForComplete() {
+  [ "$asyncDeploy" == "true" ] && wait
+  log ""
+  log "Execution $1 to all nodes finished !"
+  exit 0
+}
+
+function usage() {
+  echo "
+Usage: {start-all|stop-all|restart-all|status-all|restart-zookeeper-all|<appName> <start|stop|restart|status>}
+  start-all        Start all remote nodes apps.
+  stop-all         stop all remote nodes apps.
+  restart-all      Restart all remote nodes apps.
+  status-all       Query status all remote nodes apps.
+  <appName> <start|stop|restart|status>  For example: \$ cmdb-facade restart
+    Restart only the cmdb-facade application of all remote nodes,
+    The optionals are: zookeeper/eureka-server/iam-web/iam-facade/iam-data/cmdb-facade/cmdb-manager/... etc.
+    "
+}
+
 # ----- Main call. -----
-case $1 in
+arg1=$1
+arg2=$2
+case $arg1 in
+  help|-help|--help|-h)
+    usage
+    ;;
   status-all)
-    doCommandAll "status"
+    doCommandApps "status"
+    doCommandZookeeper "status"
+    waitForComplete "status"
     ;;
   start-all)
-    doCommandAll "start"
+    doCommandApps "start"
+    doCommandZookeeper "start"
+    waitForComplete "start"
     ;;
   stop-all)
-    doCommandAll "stop"
+    doCommandApps "stop"
+    doCommandZookeeper "stop"
+    waitForComplete "stop"
     ;;
   restart-all)
-    doCommandAll "restart"
+    doCommandApps "restart"
+    doCommandZookeeper "restart"
+    waitForComplete "restart"
+    ;;
+  *)
+    if [[ -z "$arg1" || -z "$arg2" ]]; then
+      usage
+      exit -1
+    fi
+    if [ "$arg1" == "zookeeper" ]; then
+      doCommandZookeeper "$arg2"
+    else
+      doCommandApps "$arg1" "arg2"
+    fi
+    waitForComplete "$arg1 $arg2"
     ;;
     *)
-  echo $"Usage: {start-all|stop-all|restart-all|status-all}"
-  exit 2
+  exit -1
 esac
